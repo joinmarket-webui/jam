4fb47baa2c1eb8b6424b2ab2b744491bb6a3fd92
Daniel <dnlggr@pm.me>
2022-01-26 17:20:09 +0100

    Remove unused import

--- src/components/CurrentWallet.jsx
+++ src/components/CurrentWallet.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import * as rb from "react-bootstrap";
 import DisplayAccounts from "./DisplayAccounts";
 import DisplayAccountUTXOs from "./DisplayAccountUTXOs";
