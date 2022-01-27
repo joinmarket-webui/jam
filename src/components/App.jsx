3a4f05056ba74c36ed19f296069519a34394b956
Daniel <dnlggr@pm.me>
2022-01-27 14:30:42 +0100

    Add gear icon in navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -158,12 +158,12 @@ export default function App() {
                 <rb.Offcanvas.Body>{nav}</rb.Offcanvas.Body>
               </rb.Navbar.Offcanvas>
               {currentWallet && (
-                <rb.Nav className="ms-sm-auto ms-lg-0 d-block order-sm-0">
-                  <Link to="/wallet" className="nav-link d-inline-block">
+                <rb.Nav className="ms-sm-auto ms-lg-0 d-inline-flex flex-row order-sm-0">
+                  <Link to="/wallet" className="px-2 nav-link d-inline-block">
                     {walletDisplayName(currentWallet.name)}
                   </Link>
                   <rb.Navbar.Text> · </rb.Navbar.Text>
-                  <Link to="/earn" className="nav-link d-inline-flex align-items-center">
+                  <Link to="/earn" className="px-2 nav-link d-inline-flex align-items-center">
                     Earn
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -181,22 +181,38 @@ export default function App() {
                     </svg>
                   </Link>
                   {coinjoinInProcess && (
-                    <rb.Navbar.Text>
-                      {' '}
-                      · Joining
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="1.5rem"
-                        height="1.5rem"
-                        fill="currentColor"
-                        className="ms-2"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M8 9.5a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
-                        <path d="M9.5 2c-.9 0-1.75.216-2.501.6A5 5 0 0 1 13 7.5a6.5 6.5 0 1 1-13 0 .5.5 0 0 1 1 0 5.5 5.5 0 0 0 8.001 4.9A5 5 0 0 1 3 7.5a6.5 6.5 0 0 1 13 0 .5.5 0 0 1-1 0A5.5 5.5 0 0 0 9.5 2zM8 3.5a4 4 0 1 0 0 8 4 4 0 0 0 0-8z" />
-                      </svg>
-                    </rb.Navbar.Text>
+                    <>
+                      <rb.Navbar.Text> · </rb.Navbar.Text>
+                      <rb.Navbar.Text className="px-2">
+                        Joining
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="1.5rem"
+                          height="1.5rem"
+                          fill="currentColor"
+                          className="ms-2"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M8 9.5a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
+                          <path d="M9.5 2c-.9 0-1.75.216-2.501.6A5 5 0 0 1 13 7.5a6.5 6.5 0 1 1-13 0 .5.5 0 0 1 1 0 5.5 5.5 0 0 0 8.001 4.9A5 5 0 0 1 3 7.5a6.5 6.5 0 0 1 13 0 .5.5 0 0 1-1 0A5.5 5.5 0 0 0 9.5 2zM8 3.5a4 4 0 1 0 0 8 4 4 0 0 0 0-8z" />
+                        </svg>
+                      </rb.Navbar.Text>
+                    </>
                   )}
+                  <rb.Navbar.Text> · </rb.Navbar.Text>
+                  <Link to="/settings" className="px-2 nav-link d-inline-flex align-items-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1rem"
+                      height="1rem"
+                      fill="currentColor"
+                      className="ms-0"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z" />
+                      <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z" />
+                    </svg>
+                  </Link>
                 </rb.Nav>
               )}
             </>
