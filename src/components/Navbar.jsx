97ae86b7a1f93f161b9ab38a741cdd473c7adf4b
Daniel <eggerdnl@gmail.com>
2022-02-03 00:27:37 +0100

    fix: change navbar collapse to offcanvas (#44)
    * fix: change collapse to offcanvas
    
    * Display offcanvas below header
    
    * Hide offcanvas on non-mobile
    
    * Fix undefined className in Sprite
    
    * Display nav item title on mobile
    
    * Adapt no unlocked wallet nav
    
    * Improve nav toggle button
    
    * Remove dead code
    
    Co-authored-by: Dennis Reimann <mail@dennisreimann.de>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,6 +31,88 @@ const ActivityIndicator = ({ isOn }) => {
   return <span className={`activity-indicator ${isOn ? 'activity-indicator-on' : 'activity-indicator-off'}`} />
 }
 
+const CenterNav = ({ makerRunning, onClick }) => {
+  return (
+    <rb.Nav className="justify-content-center align-items-stretch">
+      <rb.Nav.Item className="d-flex align-items-stretch">
+        <NavLink
+          to="/send"
+          onClick={onClick}
+          className={({ isActive }) =>
+            'center-nav-link nav-link d-flex align-items-center justify-content-center' + (isActive ? ' active' : '')
+          }
+        >
+          Send
+        </NavLink>
+      </rb.Nav.Item>
+      <rb.Nav.Item className="d-flex align-items-stretch">
+        <NavLink
+          to="/receive"
+          onClick={onClick}
+          className={({ isActive }) =>
+            'center-nav-link nav-link d-flex align-items-center justify-content-center' + (isActive ? ' active' : '')
+          }
+        >
+          Receive
+        </NavLink>
+      </rb.Nav.Item>
+      <rb.Nav.Item className="d-flex align-items-stretch">
+        <NavLink
+          to="/earn"
+          onClick={onClick}
+          className={({ isActive }) =>
+            'center-nav-link nav-link d-flex align-items-center justify-content-center' + (isActive ? ' active' : '')
+          }
+        >
+          <div className="d-flex align-items-start">
+            Earn
+            <ActivityIndicator isOn={makerRunning} />
+          </div>
+        </NavLink>
+      </rb.Nav.Item>
+    </rb.Nav>
+  )
+}
+
+const TrailingNav = ({ coinjoinInProcess, onClick }) => {
+  return (
+    <rb.Nav className="justify-content-center align-items-stretch">
+      {coinjoinInProcess && (
+        <rb.Nav.Item className="d-flex align-items-center justify-content-center pe-2">
+          <div className="d-flex align-items-center px-0">
+            <rb.Navbar.Text>Joining</rb.Navbar.Text>
+            <Sprite symbol="joining" width="30" height="30" className="p-1 navbar-text" />
+          </div>
+        </rb.Nav.Item>
+      )}
+      <rb.Nav.Item className="d-flex align-items-stretch">
+        <NavLink
+          to="/settings"
+          onClick={onClick}
+          className={({ isActive }) =>
+            'nav-link d-flex align-items-center justify-content-center px-0' + (isActive ? ' active' : '')
+          }
+        >
+          <Sprite symbol="gear" width="30" height="30" className="d-none d-md-inline-block" />
+          <span className="d-inline-block d-md-none">Settings</span>
+        </NavLink>
+      </rb.Nav.Item>
+      <rb.Nav.Item className="d-flex align-items-stretch">
+        <NavLink
+          to="/"
+          onClick={onClick}
+          className={({ isActive }) =>
+            'nav-link d-flex align-items-center justify-content-center px-0' + (isActive ? ' active' : '')
+          }
+        >
+          <Sprite symbol="grid" width="30" height="30" className="d-none d-md-inline-block" />
+          <span className="d-inline-block d-md-none">Wallets</span>
+        </NavLink>
+      </rb.Nav.Item>
+    </rb.Nav>
+  )
+}
+
 export default function Navbar({ connectionError, makerRunning, coinjoinInProcess }) {
   const settings = useSettings()
   const currentWallet = useCurrentWallet()
@@ -42,6 +124,7 @@ export default function Navbar({ connectionError, makerRunning, coinjoinInProces
 
   return (
     <rb.Navbar
+      id="mainNav"
       bg={settings.theme === 'light' ? 'white' : 'dark'}
       sticky="top"
       expand="md"
@@ -59,20 +142,37 @@ export default function Navbar({ connectionError, makerRunning, coinjoinInProces
           <>
             {!currentWallet ? (
               <>
-                <Link to="/" className="navbar-brand nav-link d-flex align-items-center" style={{ height: height }}>
+                <Link
+                  to="/"
+                  className="navbar-brand nav-link d-flex align-items-center ps-0 ps-sm-2 ps-xl-0"
+                  style={{ height: height }}
+                >
                   <Sprite symbol="logo" width="30" height="30" className="d-inline-block align-top" />
                   <span className="ms-2">JoinMarket</span>
                 </Link>
-                <rb.Navbar.Toggle className="border-0" />
-                <rb.Navbar.Collapse>
-                  <rb.Nav className="ms-auto">
-                    <rb.Nav.Item>
-                      <Link to="/create-wallet" onClick={() => isExpanded && setIsExpanded(false)} className="nav-link">
-                        Create Wallet
-                      </Link>
-                    </rb.Nav.Item>
-                  </rb.Nav>
-                </rb.Navbar.Collapse>
+                <div className="d-flex d-md-none align-items-center">
+                  <rb.Navbar.Toggle id="mainNavToggle">
+                    <span>Menu</span>
+                  </rb.Navbar.Toggle>
+                </div>
+                <rb.Navbar.Offcanvas className={`navbar-${settings.theme}`} placement="end">
+                  <rb.Offcanvas.Header>
+                    <rb.Offcanvas.Title>JoinMarket Web UI</rb.Offcanvas.Title>
+                  </rb.Offcanvas.Header>
+                  <rb.Offcanvas.Body>
+                    <rb.Nav className="ms-auto">
+                      <rb.Nav.Item>
+                        <Link
+                          to="/create-wallet"
+                          onClick={() => isExpanded && setIsExpanded(false)}
+                          className="nav-link"
+                        >
+                          Create Wallet
+                        </Link>
+                      </rb.Nav.Item>
+                    </rb.Nav>
+                  </rb.Offcanvas.Body>
+                </rb.Navbar.Offcanvas>
               </>
             ) : (
               <>
@@ -96,82 +196,28 @@ export default function Navbar({ connectionError, makerRunning, coinjoinInProces
                     </NavLink>
                   </rb.Nav.Item>
                 </rb.Nav>
-                <rb.Navbar.Toggle className="border-0" />
-                <rb.Navbar.Collapse className="flex-1 flex-grow-0 align-items-stretch">
-                  <rb.Nav className="justify-content-center align-items-stretch">
-                    <rb.Nav.Item className="d-flex align-items-stretch">
-                      <NavLink
-                        to="/send"
-                        onClick={() => isExpanded && setIsExpanded(false)}
-                        className={({ isActive }) =>
-                          'center-nav-link nav-link d-flex align-items-center justify-content-center' +
-                          (isActive ? ' active' : '')
-                        }
-                      >
-                        Send
-                      </NavLink>
-                    </rb.Nav.Item>
-                    <rb.Nav.Item className="d-flex align-items-stretch">
-                      <NavLink
-                        to="/receive"
-                        onClick={() => isExpanded && setIsExpanded(false)}
-                        className={({ isActive }) =>
-                          'center-nav-link nav-link d-flex align-items-center justify-content-center' +
-                          (isActive ? ' active' : '')
-                        }
-                      >
-                        Receive
-                      </NavLink>
-                    </rb.Nav.Item>
-                    <rb.Nav.Item className="d-flex align-items-stretch">
-                      <NavLink
-                        to="/earn"
-                        onClick={() => isExpanded && setIsExpanded(false)}
-                        className={({ isActive }) =>
-                          'center-nav-link  nav-link d-flex align-items-center justify-content-center' +
-                          (isActive ? ' active' : '')
-                        }
-                      >
-                        <div className="d-flex align-items-start">
-                          Earn
-                          <ActivityIndicator isOn={makerRunning} />
-                        </div>
-                      </NavLink>
-                    </rb.Nav.Item>
-                  </rb.Nav>
-                </rb.Navbar.Collapse>
-                <rb.Navbar.Collapse className="flex-1">
-                  <span className="ms-auto">
-                    <rb.Nav className="align-items-center">
-                      {coinjoinInProcess && (
-                        <rb.Nav.Item className="d-flex align-items-center pe-2">
-                          <>
-                            <rb.Navbar.Text>Joining</rb.Navbar.Text>
-                            <Sprite symbol="joining" width="30" height="30" className="text-secondary p-1" />
-                          </>
-                        </rb.Nav.Item>
-                      )}
-                      <rb.Nav.Item className="d-flex">
-                        <NavLink
-                          to="/settings"
-                          onClick={() => isExpanded && setIsExpanded(false)}
-                          className={({ isActive }) => 'nav-link px-0' + (isActive ? ' active' : '')}
-                        >
-                          <Sprite symbol="gear" width="30" height="30" />
-                        </NavLink>
-                      </rb.Nav.Item>
-                      <rb.Nav.Item className="d-flex">
-                        <NavLink
-                          to="/"
-                          onClick={() => isExpanded && setIsExpanded(false)}
-                          className={({ isActive }) => 'nav-link px-0' + (isActive ? ' active' : '')}
-                        >
-                          <Sprite symbol="grid" width="30" height="30" />
-                        </NavLink>
-                      </rb.Nav.Item>
-                    </rb.Nav>
-                  </span>
-                </rb.Navbar.Collapse>
+                <div className="d-flex d-md-none align-items-center">
+                  <rb.Navbar.Toggle id="mainNavToggle">
+                    <span>Menu</span>
+                  </rb.Navbar.Toggle>
+                </div>
+                <rb.Navbar.Offcanvas className={`navbar-${settings.theme}`} placement="end">
+                  <rb.Offcanvas.Header>
+                    <rb.Offcanvas.Title>JoinMarket Web UI</rb.Offcanvas.Title>
+                  </rb.Offcanvas.Header>
+                  <rb.Offcanvas.Body>
+                    <CenterNav makerRunning={makerRunning} onClick={() => setIsExpanded(!isExpanded)} />
+                    <TrailingNav coinjoinInProcess={coinjoinInProcess} onClick={() => setIsExpanded(!isExpanded)} />
+                  </rb.Offcanvas.Body>
+                </rb.Navbar.Offcanvas>
+                <rb.Container className="d-none d-md-flex flex-1 flex-grow-0 align-items-stretch">
+                  <CenterNav makerRunning={makerRunning} />
+                </rb.Container>
+                <rb.Container className="d-none d-md-flex flex-1 align-items-stretch">
+                  <div className="ms-auto d-flex align-items-stretch">
+                    <TrailingNav coinjoinInProcess={coinjoinInProcess} />
+                  </div>
+                </rb.Container>
               </>
             )}
           </>
--- src/components/Sprite.jsx
+++ src/components/Sprite.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Sprite({ symbol, className, ...props }) {
   return (
-    <svg role="img" className={`d-inline-block sprite sprite-${symbol} ${className}`} {...props}>
+    <svg role="img" className={`d-inline-block sprite sprite-${symbol}${className ? ` ${className}` : ''}`} {...props}>
       <use href={`${process.env.PUBLIC_URL}/sprite.svg#${symbol}`}></use>
     </svg>
   )
