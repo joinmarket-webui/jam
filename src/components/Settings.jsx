cd2f0bf98be474d60be0e142a1a7e89d55f37e44
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-02-09 03:18:11 -0700

    feat: improve wording in settings (#71)
    * Update settings wording to be more clear
    
    * Use shorter conditional
    
    Co-authored-by: Thebora Kompanioni <theborakompanioni@users.noreply.github.com>
    
    Co-authored-by: Gigi <109058+dergigi@users.noreply.github.com>
    Co-authored-by: Thebora Kompanioni <theborakompanioni@users.noreply.github.com>

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -65,7 +65,7 @@ export default function Settings({ currentWallet }) {
             height="24"
             className="me-2"
           />
-          {isLightTheme ? 'Dark' : 'Light'} theme
+          Switch to {isLightTheme ? 'dark' : 'light'} theme
         </rb.Button>
 
         <br />
