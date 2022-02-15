c09a8afab23f8192880931e7df8c420162b45cbd
Daniel <10026790+dnlggr@users.noreply.github.com>
2022-02-15 12:04:06 +0100

    Remove strange symbol

--- src/components/Earn.jsx
+++ src/components/Earn.jsx
@@ -241,7 +241,7 @@ export default function Earn({ currentWallet, coinjoinInProcess, makerRunning })
 
           <rb.Fade in={coinjoinInProcess} mountOnEnter={true} unmountOnExit={true}>
             <div className="mb-4 p-3 border border-1 rounded">
-              🛈 <small className="text-secondary">A collaborative transaction is currently in progress.</small>
+              <small className="text-secondary">A collaborative transaction is currently in progress.</small>
             </div>
           </rb.Fade>
 
