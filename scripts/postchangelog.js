cf970e9a21bb38e5ba19a1aabdf9621ffaf19686
Daniel <10026790+dnlggr@users.noreply.github.com>
2022-02-16 15:08:24 +0100

    Update to only generate changelog

--- scripts/postchangelog.js
+++
@@ -1,4 +0,0 @@
-const replace = require('replace')
-
-replace({ regex: /###\s(\w+)/, replacement: '#### $1', paths: ['./CHANGELOG.md'] })
-replace({ regex: /\n\n\n/, replacement: '\n\n', paths: ['./CHANGELOG.md'] })
