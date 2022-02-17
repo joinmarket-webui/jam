e83ebb938936f0ea6fb35c94c50aec217048005c
Daniel <10026790+dnlggr@users.noreply.github.com>
2022-02-17 11:13:00 +0100

    docs: add automated changelog generation (#103)
    * Add changelog
    
    * Update to only generate changelog
    
    * Fix version

---
+++ scripts/changelog.mjs
@@ -0,0 +1,71 @@
+import conventionalChangelog from 'conventional-changelog'
+import fs from 'fs'
+
+const pkg = JSON.parse(fs.readFileSync('./package.json'))
+
+const START_OF_LAST_RELEASE_PATTERN = /(^#+ \[?[0-9]+\.[0-9]+\.[0-9]+|<a name=)/m
+
+const file = 'CHANGELOG.md'
+
+const header = `\
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+`
+
+const types = [
+  { type: 'feat', section: 'Added' },
+  { type: 'update', section: 'Changed' },
+  { type: 'remove', section: 'Removed' },
+  { type: 'fix', section: 'Fixed' },
+  { type: 'security', section: 'Security' },
+  { type: 'chore', hidden: true },
+  { type: 'docs', hidden: true },
+  { type: 'style', hidden: true },
+  { type: 'refactor', hidden: true },
+  { type: 'perf', hidden: true },
+  { type: 'test', hidden: true },
+]
+
+const generateChangelog = (newVersion) => {
+  return new Promise((resolve, reject) => {
+    let oldContent = fs.readFileSync(file, 'utf-8')
+    const oldContentStart = oldContent.search(START_OF_LAST_RELEASE_PATTERN)
+
+    if (oldContentStart !== -1) {
+      oldContent = oldContent.substring(oldContentStart)
+    }
+
+    let changelog = ''
+
+    const changelogStream = conventionalChangelog(
+      {
+        preset: {
+          name: 'conventionalcommits',
+          header: header,
+          types: types,
+        },
+        tagPrefix: 'v',
+      },
+      { version: newVersion }
+    ).on('error', function (err) {
+      return reject(err)
+    })
+
+    changelogStream.on('data', (buffer) => {
+      changelog += buffer.toString()
+    })
+
+    changelogStream.on('end', () => {
+      changelog = changelog.replace(/###\s(\w+)/g, '#### $1').replace(/\n\n\n/g, '\n\n')
+      const finalChangelog = header + '\n' + (changelog + oldContent).replace(/\n+$/, '\n')
+      fs.writeFileSync(file, finalChangelog, 'utf8')
+      return resolve()
+    })
+  })
+}
+
+await generateChangelog(pkg.version)
