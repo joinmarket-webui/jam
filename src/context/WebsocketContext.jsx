cb94ccb756d5a6537280996c500714795a275e3f
theborakompanioni <theborakompanioni+github@gmail.com>
2022-02-23 12:49:09 +0100

    add WebsocketProvider

---
+++ src/context/WebsocketContext.jsx
@@ -0,0 +1,65 @@
+import React, { createContext, useEffect, useRef, useContext } from 'react'
+
+import { useCurrentWallet } from './WalletContext'
+
+const WebsocketContext = createContext()
+
+const WebsocketProvider = ({ children }) => {
+  const websocket = useRef(null)
+  const currentWallet = useCurrentWallet()
+
+  useEffect(() => {
+    if (websocket.current) {
+      websocket.current.close()
+    }
+
+    const { protocol, host } = window.location
+    const scheme = protocol === 'https:' ? 'wss' : 'ws'
+    websocket.current = new WebSocket(`${scheme}://${host}/jmws`)
+
+    websocket.current.onopen = () => {
+      console.debug('websocket connection openend')
+    }
+    websocket.current.onclose = () => {
+      console.debug('websocket connection closed')
+    }
+
+    websocket.current.onerror = (error) => {
+      console.error('websocket error', error)
+    }
+
+    websocket.current.onmessage = (event) => {
+      const wsdata = JSON.parse(event.data)
+      console.debug('websocket sent', wsdata)
+    }
+
+    const wsCurrent = websocket.current
+    return () => {
+      wsCurrent.close()
+    }
+  }, [])
+
+  useEffect(() => {
+    // The client must send the authentication token when it connects,
+    // otherwise it will not receive any notifications.
+    const initNotifications = () => {
+      if (websocket.current && currentWallet) {
+        websocket.current.send(currentWallet.token)
+      }
+    }
+
+    initNotifications()
+  }, [currentWallet])
+
+  return <WebsocketContext.Provider value={{ websocket: websocket.current }}>{children}</WebsocketContext.Provider>
+}
+
+const useWebsocket = () => {
+  const context = useContext(WebsocketContext)
+  if (context === undefined) {
+    throw new Error('useWebsocket must be used within a WebsocketProvider')
+  }
+  return context.websocket
+}
+
+export { WebsocketContext, WebsocketProvider, useWebsocket }
