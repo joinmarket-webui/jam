41648c47289cec95c5cb9d036289ebe6d5c1f783
Thebora Kompanioni <theborakompanioni@users.noreply.github.com>
2022-03-03 14:16:21 +0100

    chore: check for dependencies in dev setup scripts (#147)
    * chore: check for dependencies in dev setup scripts
    
    * chore: fix some shellcheck warnings in setup scripts
    
    * chore: fix SC2155 shellcheck warnings

--- docker/regtest/fund-wallet.sh
+++ docker/regtest/fund-wallet.sh
@@ -3,6 +3,11 @@
 set -Eeuo pipefail
 trap cleanup SIGINT SIGTERM ERR EXIT
 
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  # script cleanup here
+}
+
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
 . "$script_dir/common.sh"
@@ -37,11 +42,6 @@ Examples:
 
 EOF
   exit
-}
-
-cleanup() {
-  trap - SIGINT SIGTERM ERR EXIT
-  # script cleanup here
 }
 
 parse_params() {
@@ -112,15 +112,14 @@ parse_params() {
   return 0
 }
 
-setup_colors
 parse_params "$@"
 
 # ----------------------------------------
 
 msg "Trying to fund wallet $wallet_name.."
 
-[ -z $(is_docker_container_running "jm_regtest_bitcoind") ] && die "Please make sure bitcoin container 'jm_regtest_bitcoind' is running."
-[ -z $(is_docker_container_running "$container") ] && die "Please make sure joinmarket container '$container' is running."
+[ -z "$(is_docker_container_running "jm_regtest_bitcoind")" ] && die "Please make sure bitcoin container 'jm_regtest_bitcoind' is running."
+[ -z "$(is_docker_container_running "$container")" ] && die "Please make sure joinmarket container '$container' is running."
 
 
 verify_no_open_session_or_throw "$base_url"
@@ -199,7 +198,7 @@ msg_success "Successfully locked wallet $wallet_name."
 # --------------------------
 # generate new blocks with rewards to wallet
 msg "Generating $blocks blocks with rewards to $address"
-. "$script_dir/mine-block.sh" $blocks $address &>/dev/null
+. "$script_dir/mine-block.sh" "$blocks" "$address" &>/dev/null
 
 if [ "$unmatured" = true ]; then
   msg_warn "Block rewards are not yet matured and will not be immediately spendable"
