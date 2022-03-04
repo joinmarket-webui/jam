89af44b237803b4b448dd9f0ca51cc4bb5e1472d
Daniel <10026790+dnlggr@users.noreply.github.com>
2022-03-04 13:01:20 +0100

    Add exemplary german translation

--- src/i18n.js
+++ src/i18n.js
@@ -3,11 +3,15 @@ import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
 import en from './locales/en/translation.json'
+// import de from './locales/de/translation.json'
 
 const resources = {
   en: {
     translation: en,
   },
+  // de: {
+  //   translation: de,
+  // },
 }
 
 i18n.use(LanguageDetector).use(initReactI18next).init({
