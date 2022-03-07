26ffe8cdeb2146a757c90d05b67d3304485af918
Daniel <10026790+dnlggr@users.noreply.github.com>
2022-03-07 15:29:50 +0100

    fix: update suggested number of collaborators (#150)
    * fix: update suggested number of colaborators
    
    * Remove fallback
    
    * Redo calculation
    
    * Update initial calc

--- src/components/Send.jsx
+++ src/components/Send.jsx
@@ -11,7 +11,7 @@ import * as Api from '../libs/JmWalletApi'
 // initial value for `minimum_markers` from the default joinmarket.cfg (last check on 2022-02-20 of v0.9.5)
 const MINIMUM_MAKERS_DEFAULT_VAL = 4
 
-// not cryptographically random
+// not cryptographically random. returned number is in range [min, max] (both inclusive).
 const pseudoRandomNumber = (min, max) => {
   return Math.round(Math.random() * (max - min)) + min
 }
@@ -48,7 +48,10 @@ const CollaboratorsSelector = ({ numCollaborators, setNumCollaborators, minNumCo
     }
   }
 
-  const defaultCollaboratorsSelection = [0, 2, 3, 4, 6].map((val) => val + minNumCollaborators)
+  var defaultCollaboratorsSelection = [8, 9, 10]
+  if (minNumCollaborators > 8) {
+    defaultCollaboratorsSelection = [minNumCollaborators, minNumCollaborators + 1, minNumCollaborators + 2]
+  }
 
   return (
     <rb.Form noValidate className="collaborators-selector">
@@ -155,8 +158,13 @@ export default function Send({ makerRunning, coinjoinInProcess }) {
   const initialAccount = 0
   const initialAmount = null
   const initialNumCollaborators = (minValue) => {
-    // always suggest a reasonably large number even when the configured minimum is unreasonably low
-    return Math.max(MINIMUM_MAKERS_DEFAULT_VAL, minValue) + pseudoRandomNumber(1, 3)
+    const defaultNumber = pseudoRandomNumber(8, 10)
+
+    if (minValue > 8) {
+      return minValue + pseudoRandomNumber(0, 2)
+    }
+
+    return defaultNumber
   }
 
   const [destination, setDestination] = useState(initialDestination)
