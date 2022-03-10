222404ef1762196802e328abefadb2b928002d69
Daniel <10026790+dnlggr@users.noreply.github.com>
2022-03-10 16:23:03 +0100

    Use satscomma for btc formatting

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,74 +1,95 @@
 import React, { useState, useEffect } from 'react'
-import Sprite from './Sprite'
 import { BTC, SATS, btcToSats, satsToBtc } from '../utils'
+import Sprite from './Sprite'
 
-const UNIT_MODE_BTC = 0
-const UNIT_MODE_SATS = 1
-const UNIT_MODE_HIDDEN = 2
+const DISPLAY_MODE_BTC = 0
+const DISPLAY_MODE_SATS = 1
+const DISPLAY_MODE_HIDDEN = 2
 
-const getUnitMode = (unit, showBalance) => {
-  if (showBalance && unit === SATS) return UNIT_MODE_SATS
-  if (showBalance && unit === BTC) return UNIT_MODE_BTC
+const getDisplayMode = (unit, showBalance) => {
+  if (showBalance && unit === SATS) return DISPLAY_MODE_SATS
+  if (showBalance && unit === BTC) return DISPLAY_MODE_BTC
 
-  return UNIT_MODE_HIDDEN
+  return DISPLAY_MODE_HIDDEN
 }
 
-export default function Balance({ value, unit, showBalance = false }) {
-  const [unitMode, setUnitMode] = useState(getUnitMode(unit, showBalance))
-
-  useEffect(() => {
-    setUnitMode(getUnitMode(unit, showBalance))
-  }, [unit, showBalance])
-
-  if (unitMode === UNIT_MODE_HIDDEN) {
-    return (
-      <span className="balance-wrapper">
-        <span className="balance slashed-zeroes">
-          <span>*****</span>
-          <span className="text-muted d-inline-flex align-items-center">
-            <Sprite symbol="hide" width="1.2em" height="1.2em" className="ps-1" />
-          </span>
-        </span>
-      </span>
-    )
-  }
-
-  const btcFormatter = new Intl.NumberFormat('en-US', {
+const formatBtc = (value) => {
+  const formatter = new Intl.NumberFormat('en-US', {
     minimumIntegerDigits: 1,
     minimumFractionDigits: 8,
   })
 
-  const satFormatter = new Intl.NumberFormat('en-US', {
+  const numberString = formatter.format(value)
+
+  const decimalPoint = '\u002E'
+  const nbHalfSpace = '\u202F'
+
+  const [integerPart, fractionalPart] = numberString.split(decimalPoint)
+
+  const formattedFractionalPart = fractionalPart
+    .split('')
+    .map((char, idx) => (idx === 2 || idx === 5 ? `${nbHalfSpace}${char}` : char))
+    .join('')
+
+  return integerPart + decimalPoint + formattedFractionalPart
+}
+
+const formatSats = (value) => {
+  const formatter = new Intl.NumberFormat('en-US', {
     minimumIntegerDigits: 1,
     minimumFractionDigits: 0,
   })
 
-  const isSats = value === parseInt(value)
-  const isBTC = !isSats && typeof value === 'string' && value.indexOf('.') > -1
+  return formatter.format(value)
+}
 
-  const btcSymbol = <span className="bitcoin-symbol">{'\u20BF'}</span>
-  const satSymbol = <Sprite symbol="sats" width="20px" height="20px" />
+const BalanceComponent = ({ symbol, value, symbolIsPrefix }) => {
+  return (
+    <span className="d-inline-flex align-items-center">
+      {symbolIsPrefix && symbol}
+      <span className="d-inline-flex align-items-center slashed-zeroes">{value}</span>
+      {!symbolIsPrefix && symbol}
+    </span>
+  )
+}
 
-  const balanceJSX = (symbolJSX, formattedValue, isPrefix = true) => {
+export default function Balance({ value, unit, showBalance = false }) {
+  const [unitMode, setUnitMode] = useState(getDisplayMode(unit, showBalance))
+
+  useEffect(() => {
+    setUnitMode(getDisplayMode(unit, showBalance))
+  }, [unit, showBalance])
+
+  if (unitMode === DISPLAY_MODE_HIDDEN) {
     return (
-      <span className="balance-wrapper">
-        {isPrefix ? symbolJSX : ''}
-        <span className="balance slashed-zeroes">{formattedValue}</span>
-        {!isPrefix ? symbolJSX : ''}
-      </span>
+      <BalanceComponent
+        symbol={
+          <span className="d-inline-flex align-items-center text-muted">
+            <Sprite symbol="hide" width="1.2em" height="1.2em" className="ps-1" />
+          </span>
+        }
+        value={'*****'}
+        symbolIsPrefix={false}
+      />
     )
   }
 
-  if (isBTC && unitMode === UNIT_MODE_BTC) return balanceJSX(btcSymbol, btcFormatter.format(value))
-  if (isSats && unitMode === UNIT_MODE_SATS) return balanceJSX(satSymbol, satFormatter.format(value), false)
+  const valueIsSats = value === parseInt(value)
+  const valueIsBtc = !valueIsSats && typeof value === 'string' && value.indexOf('.') > -1
 
-  if (isBTC && unitMode === UNIT_MODE_SATS) return balanceJSX(satSymbol, satFormatter.format(btcToSats(value)), false)
-  if (isSats && unitMode === UNIT_MODE_BTC) return balanceJSX(btcSymbol, btcFormatter.format(satsToBtc(value)))
+  const btcSymbol = <span style={{ paddingRight: '0.1em' }}>{'\u20BF'}</span>
+  const satSymbol = <Sprite symbol="sats" width="1.2em" height="1.2em" />
+
+  if (valueIsBtc && unitMode === DISPLAY_MODE_BTC)
+    return <BalanceComponent symbol={btcSymbol} value={formatBtc(value)} symbolIsPrefix={true} />
+  if (valueIsSats && unitMode === DISPLAY_MODE_SATS)
+    return <BalanceComponent symbol={satSymbol} value={formatSats(value)} symbolIsPrefix={false} />
+
+  if (valueIsBtc && unitMode === DISPLAY_MODE_SATS)
+    return <BalanceComponent symbol={satSymbol} value={formatSats(btcToSats(value))} symbolIsPrefix={false} />
+  if (valueIsSats && unitMode === DISPLAY_MODE_BTC)
+    return <BalanceComponent symbol={btcSymbol} value={formatBtc(satsToBtc(value))} symbolIsPrefix={true} />
 
   // Something unexpected happened. Simply render what was passed in the props.
-  return (
-    <span className="balance">
-      {value} {unit}
-    </span>
-  )
+  return <BalanceComponent symbol={unit} value={value} symbolIsPrefix={false} />
 }
