ff0201e6a89c55fc829b569baec7402529dd1f70
Daniel <10026790+dnlggr@users.noreply.github.com>
2022-03-11 14:13:08 +0100

    Translate onboarding

--- src/components/Onboarding.jsx
+++ src/components/Onboarding.jsx
@@ -1,9 +1,11 @@
 import * as rb from 'react-bootstrap'
 import React, { useState } from 'react'
+import { Trans, useTranslation } from 'react-i18next'
 import Sprite from './Sprite'
 import { useSettingsDispatch } from '../context/SettingsContext'
 
 export default function Onboarding() {
+  const { t } = useTranslation()
   const [step, setStep] = useState(0)
   const settingsDispatch = useSettingsDispatch()
 
@@ -17,33 +19,28 @@ export default function Onboarding() {
 
   const steps = [
     {
-      title: 'Welcome to Jam for JoinMarket!',
-      description:
-        'JoinMarket is a privacy-focused software solution that aims to improve the confidentiality and privacy of your bitcoin transactions. It facilitates the creation of collaborative transactions through a peer-to-peer marketplace.',
+      title: t('onboarding.screen_1_title'),
+      description: t('onboarding.screen_1_description'),
       icon: 'welcome',
     },
     {
-      title: 'Collaborative Transactions',
-      description:
-        'To have strong privacy guarantees in the open and transparent world of bitcoin, special kinds of transactions have to be created. JoinMarket helps you to create these transactions in an easy and automated way.',
+      title: t('onboarding.screen_2_title'),
+      description: t('onboarding.screen_2_description'),
       icon: 'collab',
     },
     {
-      title: 'You Are In Control',
-      description:
-        'JoinMarket is fully non-custodial, meaning that you always have full control over your funds. The system uses Bitcoin’s smart contracts to make sure that all transactions are atomic and your funds are secure at all times.',
+      title: t('onboarding.screen_3_title'),
+      description: t('onboarding.screen_3_description'),
       icon: 'key',
     },
     {
-      title: 'No Trusted Third Parties',
-      description:
-        'Since JoinMarket is a peer-to-peer system, trusted third parties are eliminated from the get-go. This unique market-driven approach reduces counterparty risk to a minimum.',
+      title: t('onboarding.screen_4_title'),
+      description: t('onboarding.screen_4_description'),
       icon: 'handshake',
     },
     {
-      title: 'Privacy for All',
-      description:
-        'JoinMarket is free and open-source software without a single point of failure. Everyone is free to use it and build upon it.',
+      title: t('onboarding.screen_5_title'),
+      description: t('onboarding.screen_5_description'),
       icon: 'shield-outline',
     },
   ]
@@ -54,35 +51,37 @@ export default function Onboarding() {
         <div className="d-flex justify-content-center">
           <Sprite symbol="logo" width="8rem" height="8rem" className="mb-4" />
         </div>
-        <h1 className="text-center">Jam</h1>
-        <h2 className="text-center fw-normal text-secondary mb-5">A friendly UI for JoinMarket</h2>
+        <h1 className="text-center">{t('onboarding.splashscreen_title')}</h1>
+        <h2 className="text-center fw-normal text-secondary mb-5">{t('onboarding.splashscreen_subtitle')}</h2>
         <div className="d-flex justify-content-center mb-2">
           <rb.Button size="lg" variant="dark" onClick={next}>
-            Get started
+            {t('onboarding.splashscreen_button_get_started')}
           </rb.Button>
         </div>
         <div className="d-flex justify-content-center">
           <rb.Button size="lg" variant="outline-dark" onClick={() => settingsDispatch({ showOnboarding: false })}>
-            Skip intro
+            {t('onboarding.splashscreen_button_skip_intro')}
           </rb.Button>
         </div>
         <p className="mt-4 text-center text-secondary">
-          Your wallet, your coins. <br />
-          100% open-source & open-design.
+          {t('onboarding.splashscreen_description_line1')}
+          <br />
+          {t('onboarding.splashscreen_description_line2')}
         </p>
         <div className="mt-4 text-secondary text-center">
-          <div className="fw-bolder">Warning</div>
+          <div className="fw-bolder">{t('onboarding.splashscreen_warning_title')}</div>
           <div>
-            While JoinMarket is tried and tested, Jam is not. It is in an alpha stage, so use with caution.
-            <a
-              href="https://github.com/joinmarket-webui/joinmarket-webui/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="link-secondary"
-            >
-              Help us improve the project on GitHub.
-            </a>
-            on GitHub.
+            <Trans i18nKey="onboarding.splashscreen_warning_text">
+              While JoinMarket is tried and tested, Jam is not. It is in an alpha stage, so use with caution.
+              <a
+                href="https://github.com/joinmarket-webui/joinmarket-webui/issues"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="link-secondary"
+              >
+                Help us improve the project on GitHub.
+              </a>
+            </Trans>
           </div>
         </div>
       </div>
@@ -101,7 +100,7 @@ export default function Onboarding() {
         </div>
         <div className="button d-flex justify-content-center">
           <rb.Button size="lg" variant="dark" onClick={next}>
-            {step === steps.length ? "Let's go!" : 'Next'}
+            {step === steps.length ? t('onboarding.button_complete') : t('onboarding.button_next')}
           </rb.Button>
         </div>
       </>
