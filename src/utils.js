e2404f2c07bc2321a36e9f725ba2dffd5f594c05
Daniel <10026790+dnlggr@users.noreply.github.com>
2022-03-15 13:26:43 +0100

    Remove dead code

--- src/utils.js
+++ src/utils.js
@@ -4,18 +4,6 @@ export const SATS = 'sats'
 
 export const serialize = (form) => Object.fromEntries(new FormData(form).entries())
 
-export const dasherize = (string) => String(string).replace(/\W+/gi, '-')
-
-export const lowercaseFirstChar = (string) => string.charAt(0).toLowerCase() + string.slice(1)
-
-export const upcaseFirstChar = (string) => string.charAt(0).toUpperCase() + string.slice(1)
-
-export const titleize = (string) =>
-  string
-    .split(/\W+/gi)
-    .map((w) => upcaseFirstChar(w))
-    .join(' ')
-
 export const walletDisplayName = (name) => name.replace('.jmdat', '')
 
 export const displayDate = (string) => new Date(string).toLocaleString()
