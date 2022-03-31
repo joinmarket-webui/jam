80fe0922abfee16fee0f1ac66450bc3ce589412b
theborakompanioni <theborakompanioni+github@gmail.com>
2022-03-31 17:49:24 +0200

    ui: adapt connection indicator icon to match figma more closely

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,6 +18,7 @@ import { useCurrentWallet, useSetCurrentWallet, useSetCurrentWalletInfo } from '
 import { useSessionConnectionError } from '../context/ServiceInfoContext'
 import { setSession, clearSession } from '../session'
 import Onboarding from './Onboarding'
+import Sprite from './Sprite'
 
 export default function App() {
   const { t } = useTranslation()
@@ -172,7 +173,12 @@ export default function App() {
             </rb.Nav.Item>
           </div>
           <div className="d-flex order-0 order-md-2 flex-1 justify-content-center justify-content-md-end align-items-center">
-            <span className={`mx-1 ${websocketConnected ? 'text-success' : 'text-danger'}`}>•</span>
+            <span
+              className={`mx-1 ${websocketConnected ? 'text-success' : 'text-danger'}`}
+              data-testid="connection-indicator-icon"
+            >
+              <Sprite symbol="node" width="24px" height="24px" />
+            </span>
             <span className="text-secondary">
               {websocketConnected ? t('footer.connected') : t('footer.disconnected')}
             </span>
