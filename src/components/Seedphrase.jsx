95e3122aeb739108c638a04b61c885a92e89059c
Djo1e <djordjespasic7@gmail.com>
2022-04-20 12:48:37 +0700

    Move Seedphrase css into module

--- src/components/Seedphrase.jsx
+++ src/components/Seedphrase.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import './Seedphrase.css'
+import styles from './Seedphrase.module.css'
 
 export default function Seedphrase({ seedphrase, isBlurred = true, centered = false }) {
   return (
     <div
-      className={`seedphrase slashed-zeroes d-flex flex-wrap ${
+      className={`${styles.container} slashed-zeroes d-flex flex-wrap ${
         centered ? 'justify-content-center align-items-center' : ''
       }`}
     >
       {seedphrase.split(' ').map((seedWord, index) => (
-        <div key={index} className="d-flex py-2 ps-2 pe-3">
-          <span className="seedword-index text-secondary text-end">{index + 1}</span>
+        <div key={index} className={`${styles.item} d-flex py-2 ps-2 pe-3`}>
+          <span className={`${styles['item-index']} text-secondary text-end`}>{index + 1}</span>
           <span className="text-secondary">.&nbsp;</span>
           <span className={isBlurred ? 'blurred-text' : ''}>{isBlurred ? 'abcdef' : seedWord}</span>
         </div>
