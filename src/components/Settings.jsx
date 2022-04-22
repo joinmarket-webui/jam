69bcd41dbeb02d65f41c631aa804a8248717b167
Daniel <10026790+dnlggr@users.noreply.github.com>
2022-04-22 18:18:10 +0200

    Add jm twitter to settings

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -199,6 +199,12 @@ export default function Settings() {
               {t('settings.telegram')}
             </div>
           </a>
+          <a href="https://twitter.com/joinmarket" target="_blank" rel="noopener noreferrer" className="link-dark">
+            <div className="d-flex align-items-center">
+              <Sprite symbol="twitter" width="24" height="24" className="me-2 p-1" />
+              {t('settings.jm_twitter')}
+            </div>
+          </a>
         </div>
       </div>
     </div>
