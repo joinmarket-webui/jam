c63bd252487297ecb17ba67852006d26fbdc9cc3
Thebora Kompanioni <theborakompanioni@users.noreply.github.com>
2022-05-16 17:20:41 +0200

    dev: regtest config alignment (#268)
    * dev: use same fee settings as jam release in regtest setup
    
    * dev: use default value for 'taker_utxo_retries' in regtest setup
    
    * dev: use default value for 'taker_utxo_age' in regtest setup
    
    * fix: move comment to own line in regtest default.cfg

--- docker/regtest/init-setup.sh
+++ docker/regtest/init-setup.sh
@@ -22,8 +22,8 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 # this will get more coins than the primary one in order to successfully run the tumbler.py script
 . "$script_dir/fund-wallet.sh" --container jm_regtest_joinmarket2 --unmatured --blocks 50
 
-# make block rewards spendable
-. "$script_dir/mine-block.sh" 110 &>/dev/null
+# make block rewards spendable: 100 + 5 (default of `taker_utxo_age`) + 1 = 106
+. "$script_dir/mine-block.sh" 106 &>/dev/null
 
 
 base_url='https://localhost:29183'
