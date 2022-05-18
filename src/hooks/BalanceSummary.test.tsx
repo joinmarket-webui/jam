06e602d3b2e5b8d0b15b46ca328bb6b8ba0e03db
theborakompanioni <theborakompanioni+github@gmail.com>
2022-05-18 16:53:13 +0200

    test: minimal test for calculated balances for BalanceSummary

--- src/hooks/BalanceSummary.test.tsx
+++ src/hooks/BalanceSummary.test.tsx
@@ -95,4 +95,79 @@ describe('BalanceSummary', () => {
     expect(balanceSummary.totalBalance).toBe('1.0000002')
     expect(balanceSummary.availableBalanceDontUseYet).toBe('0.00000001')
   })
+
+  it('should populate properties calculated from utxo data', () => {
+    const data = {}
+
+    act(() => {
+      Object.assign(
+        data,
+        setup({
+          data: {
+            utxos: {
+              utxos: [
+                {
+                  address: '1',
+                  path: '1',
+                  label: '1',
+                  value: 1,
+                  tries: 0,
+                  tries_remaining: 0,
+                  external: false,
+                  mixdepth: 0,
+                  confirmations: 0,
+                  frozen: false,
+                  utxo: '1',
+                  locktime: undefined,
+                },
+                {
+                  address: '2',
+                  path: '2',
+                  label: '2 - locked',
+                  value: 2,
+                  tries: 0,
+                  tries_remaining: 0,
+                  external: false,
+                  mixdepth: 0,
+                  confirmations: 0,
+                  frozen: false,
+                  utxo: '2',
+                  locktime: '2099-12',
+                },
+                {
+                  address: '3',
+                  path: '3',
+                  label: '3 - frozen',
+                  value: 3,
+                  tries: 0,
+                  tries_remaining: 0,
+                  external: false,
+                  mixdepth: 0,
+                  confirmations: 0,
+                  frozen: true,
+                  utxo: '3',
+                },
+              ],
+            },
+            display: {
+              walletinfo: {
+                wallet_name: 'test.jmdat',
+                total_balance: '0.00000001 (0.00000006)',
+                accounts: [],
+              },
+            },
+          },
+        })
+      )
+    })
+
+    const balanceSummary = data as WalletBalanceSummary
+
+    expect(balanceSummary).toBeDefined()
+    expect(balanceSummary.totalBalance).toBe('0.00000006')
+    expect(balanceSummary.availableBalanceDontUseYet).toBe('0.00000001')
+    expect(balanceSummary.calculatedAvailableBalanceInSats).toBe(1)
+    expect(balanceSummary.calculatedFrozenOrLockedBalanceInSats).toBe(5)
+    expect(balanceSummary.accountBalances).toEqual([])
+  })
 })
