6f44c5457c65714d4789e2800f4a0e778ab77bc6
theborakompanioni <theborakompanioni+github@gmail.com>
2022-06-08 13:31:53 +0200

    ui: remove account/utxo checkbox overlay

--- src/components/fidelity_bond/AccountCheckbox.tsx
+++ src/components/fidelity_bond/AccountCheckbox.tsx
@@ -22,65 +22,52 @@ interface AccountCheckboxProps {
 const AccountCheckbox = ({ account, onChange, checked, percentage }: AccountCheckboxProps) => {
   return (
     <>
-      <rb.OverlayTrigger
-        trigger="focus"
-        placement="top"
-        overlay={
-          <rb.Popover>
-            <rb.Popover.Header as="h3">{account.account}</rb.Popover.Header>
-            <rb.Popover.Body>
-              <div className="slashed-zeroes">{account.account_balance}</div>
-            </rb.Popover.Body>
-          </rb.Popover>
-        }
+      <rb.Card
+        text={checked ? 'success' : undefined}
+        border={checked ? 'success' : undefined}
+        className="w-100"
+        onClick={(e) => {
+          e.stopPropagation()
+          if (!account.disabled) {
+            onChange(account, !checked)
+          }
+        }}
+        style={{ cursor: 'pointer', position: 'relative' }}
       >
-        <rb.Card
-          text={checked ? 'success' : undefined}
-          border={checked ? 'success' : undefined}
-          className="w-100"
-          onClick={(e) => {
-            e.stopPropagation()
-            if (!account.disabled) {
-              onChange(account, !checked)
-            }
-          }}
-          style={{ cursor: 'pointer', position: 'relative' }}
-        >
-          {percentage !== undefined && <PercentageBar percentage={percentage} highlight={checked} />}
-          <rb.Card.Body>
-            <div className="d-flex align-items-center">
-              <div
-                className="d-flex align-items-center justify-content-center me-3"
-                style={{
-                  width: '3rem',
-                  height: '3rem',
-                  backgroundColor: `${checked ? 'rgba(39, 174, 96, 1)' : 'rgba(222, 222, 222, 1)'}`,
-                  color: `${checked ? 'white' : 'rgba(66, 66, 66, 1)'}`,
-                  borderRadius: '50%',
-                }}
-              >
-                {checked && <Sprite symbol="checkmark" width="24" height="24" />}
-                {!checked && account.disabled && <Sprite symbol="cross" width="24" height="24" />}
-              </div>
-
-              <rb.Stack className="align-items-start">
-                <div>Jar #{account.account}</div>
-                <div>
-                  <Balance valueString={account.account_balance} convertToUnit={SATS} showBalance={true} />
-                </div>
-                <div>
-                  <small className="text-secondary">{account.utxos.length} output(s)</small>
-                  {percentage !== undefined && (
-                    <>
-                      <small className="ps-1 text-secondary">| {`${percentage.toFixed(2)}%`}</small>
-                    </>
-                  )}
-                </div>
-              </rb.Stack>
+        {percentage !== undefined && <PercentageBar percentage={percentage} highlight={checked} />}
+        <rb.Card.Body>
+          <div className="d-flex align-items-center">
+            <div
+              className="d-flex align-items-center justify-content-center me-3"
+              style={{
+                width: '3rem',
+                height: '3rem',
+                backgroundColor: `${checked ? 'rgba(39, 174, 96, 1)' : 'rgba(222, 222, 222, 1)'}`,
+                color: `${checked ? 'white' : 'rgba(66, 66, 66, 1)'}`,
+                borderRadius: '50%',
+              }}
+            >
+              {checked && <Sprite symbol="checkmark" width="24" height="24" />}
+              {!checked && account.disabled && <Sprite symbol="cross" width="24" height="24" />}
             </div>
-          </rb.Card.Body>
-        </rb.Card>
-      </rb.OverlayTrigger>
+
+            <rb.Stack className="align-items-start">
+              <div>Jar #{account.account}</div>
+              <div>
+                <Balance valueString={account.account_balance} convertToUnit={SATS} showBalance={true} />
+              </div>
+              <div>
+                <small className="text-secondary">{account.utxos.length} output(s)</small>
+                {percentage !== undefined && (
+                  <>
+                    <small className="ps-1 text-secondary">| {`${percentage.toFixed(2)}%`}</small>
+                  </>
+                )}
+              </div>
+            </rb.Stack>
+          </div>
+        </rb.Card.Body>
+      </rb.Card>
     </>
   )
 }
--- src/components/fidelity_bond/UtxoCheckbox.tsx
+++ src/components/fidelity_bond/UtxoCheckbox.tsx
@@ -21,66 +21,53 @@ const UtxoCheckbox = ({ utxo, checked, onChange, percentage }: UtxoCheckboxProps
 
   return (
     <>
-      <rb.OverlayTrigger
-        trigger={['hover', 'focus']}
-        placement="top"
-        overlay={
-          <rb.Popover>
-            <rb.Popover.Header as="h3">{utxo.value}</rb.Popover.Header>
-            <rb.Popover.Body>
-              <div className="slashed-zeroes">{utxo.utxo}</div>
-            </rb.Popover.Body>
-          </rb.Popover>
-        }
+      <rb.Card
+        text={checked ? 'success' : undefined}
+        border={checked ? 'success' : undefined}
+        className="w-100"
+        onClick={(e) => {
+          e.stopPropagation()
+          onChange(utxo, !checked)
+        }}
+        style={{ cursor: 'pointer', position: 'relative' }}
       >
-        <rb.Card
-          text={checked ? 'success' : undefined}
-          border={checked ? 'success' : undefined}
-          className="w-100"
-          onClick={(e) => {
-            e.stopPropagation()
-            onChange(utxo, !checked)
-          }}
-          style={{ cursor: 'pointer', position: 'relative' }}
-        >
-          {percentage !== undefined && <PercentageBar percentage={percentage} highlight={checked} />}
-          <rb.Card.Body style={{}}>
-            <div className="d-flex align-items-center">
-              <div
-                className="d-flex align-items-center justify-content-center me-3"
-                style={{
-                  width: '3rem',
-                  height: '3rem',
-                  backgroundColor: `${checked ? 'rgba(39, 174, 96, 1)' : 'rgba(222, 222, 222, 1)'}`,
-                  color: 'white',
-                  borderRadius: '50%',
-                }}
-              >
-                {checked && <Sprite symbol="checkmark" width="24" height="24" />}
-              </div>
-
-              <rb.Stack className="align-items-start">
-                <rb.Form.Check type="checkbox" className="d-none" label="" checked={checked} readOnly />
-                <Balance valueString={`${utxo.value}`} convertToUnit={SATS} showBalance={true} />
-
-                {percentage !== undefined && <div>{`${percentage.toFixed(2)}%`}</div>}
-                <div>
-                  <small className="text-secondary">{utxo.confirmations} Confirmations</small>
-                </div>
-                <div>
-                  {utxo.label && <span className="me-2 badge bg-light">{utxo.label}</span>}
-                  {utxo.frozen && (
-                    <>
-                      <span className="me-2 badge bg-info">{t('current_wallet_advanced.label_frozen')}</span>
-                      {checked && <span className="small text-warning">Will be unfrozen automatically</span>}
-                    </>
-                  )}
-                </div>
-              </rb.Stack>
+        {percentage !== undefined && <PercentageBar percentage={percentage} highlight={checked} />}
+        <rb.Card.Body style={{}}>
+          <div className="d-flex align-items-center">
+            <div
+              className="d-flex align-items-center justify-content-center me-3"
+              style={{
+                width: '3rem',
+                height: '3rem',
+                backgroundColor: `${checked ? 'rgba(39, 174, 96, 1)' : 'rgba(222, 222, 222, 1)'}`,
+                color: 'white',
+                borderRadius: '50%',
+              }}
+            >
+              {checked && <Sprite symbol="checkmark" width="24" height="24" />}
             </div>
-          </rb.Card.Body>
-        </rb.Card>
-      </rb.OverlayTrigger>
+
+            <rb.Stack className="align-items-start">
+              <rb.Form.Check type="checkbox" className="d-none" label="" checked={checked} readOnly />
+              <Balance valueString={`${utxo.value}`} convertToUnit={SATS} showBalance={true} />
+
+              {percentage !== undefined && <div>{`${percentage.toFixed(2)}%`}</div>}
+              <div>
+                <small className="text-secondary">{utxo.confirmations} Confirmations</small>
+              </div>
+              <div>
+                {utxo.label && <span className="me-2 badge bg-light">{utxo.label}</span>}
+                {utxo.frozen && (
+                  <>
+                    <span className="me-2 badge bg-info">{t('current_wallet_advanced.label_frozen')}</span>
+                    {checked && <span className="small text-warning">Will be unfrozen automatically</span>}
+                  </>
+                )}
+              </div>
+            </rb.Stack>
+          </div>
+        </rb.Card.Body>
+      </rb.Card>
     </>
   )
 }
