67e23759334bd6dc4adc256bb26cc0ddf3ec05bc
Daniel <10026790+dnlggr@users.noreply.github.com>
2022-06-15 15:51:42 +0200

    feat: make jars interactive

--- src/components/Jars.jsx
+++ src/components/Jars.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import * as rb from 'react-bootstrap'
+import { useTranslation } from 'react-i18next'
 import { useSettings } from '../context/SettingsContext'
 import styles from './Jars.module.css'
 import Sprite from './Sprite'
@@ -6,22 +8,50 @@ import Balance from './Balance'
 
 const Jar = ({ accountIndex, balance, fill, onClick }) => {
   const settings = useSettings()
+  const { t } = useTranslation()
+
+  const [jarIsOpen, setJarIsOpen] = useState(false)
+  const tooltipTarget = useRef(null)
 
   const jarSymbol = ((fill) => {
     switch (fill) {
       case 1:
-        return 'jar-closed-fill-25'
+        return jarIsOpen ? 'jar-open-fill-25' : 'jar-closed-fill-25'
       case 2:
-        return 'jar-closed-fill-50'
+        return jarIsOpen ? 'jar-open-fill-50' : 'jar-closed-fill-50'
       case 3:
-        return 'jar-closed-fill-75'
+        return jarIsOpen ? 'jar-open-fill-75' : 'jar-closed-fill-75'
       default:
-        return 'jar-closed-empty'
+        return jarIsOpen ? 'jar-open-empty' : 'jar-closed-empty'
     }
   })(fill)
 
+  const onMouseOver = () => setJarIsOpen(true)
+  const onMouseOut = () => setJarIsOpen(false)
+
   return (
-    <div className="d-flex flex-column align-items-center gap-1">
+    <div ref={tooltipTarget} className={styles['jar-container']} onMouseOver={onMouseOver} onMouseOut={onMouseOut}>
+      <rb.Overlay
+        target={tooltipTarget.current}
+        show={jarIsOpen}
+        placement="top"
+        popperConfig={{
+          modifiers: [
+            {
+              name: 'offset',
+              options: {
+                offset: [0, 5],
+              },
+            },
+          ],
+        }}
+      >
+        {(props) => (
+          <rb.Tooltip id="jar-tooltip" {...props}>
+            {t('current_wallet.jar_tooltip')}
+          </rb.Tooltip>
+        )}
+      </rb.Overlay>
       <Sprite className={styles['jar-sprite']} symbol={jarSymbol} width="32px" height="48px" onClick={onClick} />
       <div className={styles['jar-index']}>{'#' + accountIndex}</div>
       <div className={styles['jar-balance']}>
@@ -32,6 +62,8 @@ const Jar = ({ accountIndex, balance, fill, onClick }) => {
 }
 
 const Jars = ({ accountBalances, totalBalance, onClick }) => {
+  const { t } = useTranslation()
+
   const sortedAccountBalances = (accountBalances || []).sort((lhs, rhs) => lhs.accountIndex - rhs.accountIndex)
 
   // Classifies the account balance into one of four groups:
@@ -47,19 +79,33 @@ const Jars = ({ accountBalances, totalBalance, onClick }) => {
     return 0
   }
 
+  const jarsDescriptionPopover = (
+    <rb.Popover id="popover-basic">
+      <rb.Popover.Body>{t('current_wallet.jars_title_popover')}</rb.Popover.Body>
+    </rb.Popover>
+  )
+
   return (
-    <div className={styles['jars-container']}>
-      {sortedAccountBalances.map((account) => {
-        return (
-          <Jar
-            key={account.accountIndex}
-            accountIndex={account.accountIndex}
-            balance={account.totalBalance}
-            fill={calculateFillLevel(account.totalBalance, totalBalance)}
-            onClick={() => onClick(account.accountIndex)}
-          />
-        )
-      })}
+    <div className="d-flex flex-column align-items-center gap-5">
+      <rb.OverlayTrigger placement="top" overlay={jarsDescriptionPopover}>
+        <div className={styles['jars-title']}>
+          <div>{t('current_wallet.jars_title')}</div>
+          <Sprite className={styles['info-icon']} symbol="info" width="18" height="18" />
+        </div>
+      </rb.OverlayTrigger>
+      <div className={styles['jars-container']}>
+        {sortedAccountBalances.map((account) => {
+          return (
+            <Jar
+              key={account.accountIndex}
+              accountIndex={account.accountIndex}
+              balance={account.totalBalance}
+              fill={calculateFillLevel(account.totalBalance, totalBalance)}
+              onClick={() => onClick(account.accountIndex)}
+            />
+          )
+        })}
+      </div>
     </div>
   )
 }
