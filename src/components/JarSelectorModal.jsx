1f35ecaef503e30871be6f5787cee599ac3c4503
Daniel <10026790+dnlggr@users.noreply.github.com>
2022-06-22 13:45:28 +0200

    refactor: extract jar into reusable component

--- src/components/JarSelectorModal.jsx
+++ src/components/JarSelectorModal.jsx
@@ -1,45 +1,9 @@
 import React, { useState, useMemo } from 'react'
 import * as rb from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
-import { useSettings } from '../context/SettingsContext'
 import styles from './JarSelectorModal.module.css'
 import Sprite from './Sprite'
-import Balance from './Balance'
-
-const Jar = ({ accountIndex, balance, isSelectable, isSelected, fill, onClick }) => {
-  const settings = useSettings()
-
-  const jarSymbol = ((fill) => {
-    switch (fill) {
-      case 1:
-        return 'jar-closed-fill-25'
-      case 2:
-        return 'jar-closed-fill-50'
-      case 3:
-        return 'jar-closed-fill-75'
-      default:
-        return 'jar-closed-empty'
-    }
-  })(fill)
-
-  return (
-    <div
-      className={`${styles['jar-container']} ${!isSelectable && styles['not-selectable']}`}
-      onClick={() => isSelectable && onClick()}
-    >
-      <Sprite className={styles['jar-sprite']} symbol={jarSymbol} width="32px" height="48px" />
-      <div className={styles['jar-index']}>{'#' + accountIndex}</div>
-      <div className={styles['jar-balance']}>
-        <Balance valueString={balance} convertToUnit={settings.unit} showBalance={settings.showBalance} />
-      </div>
-      {isSelectable ? (
-        <div className={`${styles['selection-circle']} ${isSelected && styles['selection-circle-filled']}`}></div>
-      ) : (
-        <div className={styles['selection-circle-placeholder']}></div>
-      )}
-    </div>
-  )
-}
+import { calculateFillLevel, SelectableJar } from './jars/Jar'
 
 export default function JarSelectorModal({
   isShown,
@@ -50,27 +14,14 @@ export default function JarSelectorModal({
   onCancel,
   onConfirm,
 }) {
+  const { t } = useTranslation()
+
   const [selectedJar, setSelectedJar] = useState(null)
 
   const sortedAccountBalances = useMemo(() => {
     if (!accountBalances) return []
     return Object.values(accountBalances).sort((lhs, rhs) => lhs.accountIndex - rhs.accountIndex)
   }, [accountBalances])
-
-  // Classifies the account balance into one of four groups:
-  // - More than half of the total balance
-  // - More than a quarter of the total balance
-  // - Not empty
-  // - Empty
-  const calculateFillLevel = (accountBalance, totalBalance) => {
-    if (!totalBalance) return 0
-
-    if (accountBalance > totalBalance / 2) return 3
-    if (accountBalance > totalBalance / 4) return 2
-    if (accountBalance > 0) return 1
-
-    return 0
-  }
 
   const cancel = () => {
     setSelectedJar(null)
@@ -88,44 +39,44 @@ export default function JarSelectorModal({
       show={isShown}
       keyboard={true}
       onEscapeKeyDown={onCancel}
+      onHide={onCancel}
       centered={true}
       animation={true}
-      backdrop="static"
-      className={styles['modal']}
+      className={styles.modal}
     >
-      <rb.Modal.Header className={styles['modal-header']}>
-        <rb.Modal.Title className={styles['modal-title']}>
+      <rb.Modal.Header className={styles.modalHeader}>
+        <rb.Modal.Title className={styles.modalTitle}>
           <div>
             <div>{title}</div>
-            <rb.Button onClick={cancel} className={styles['cancel-button']}>
+            <rb.Button onClick={cancel} className={styles.cancelButton}>
               <Sprite symbol="cancel" width="26" height="26" />
             </rb.Button>
           </div>
         </rb.Modal.Title>
       </rb.Modal.Header>
-      <rb.Modal.Body className={styles['modal-body']}>
-        <div className={styles['jars-container']}>
+      <rb.Modal.Body className={styles.modalBody}>
+        <div className={styles.jarsContainer}>
           {sortedAccountBalances.map((account) => {
             return (
-              <Jar
+              <SelectableJar
                 key={account.accountIndex}
-                accountIndex={account.accountIndex}
+                index={account.accountIndex}
                 balance={account.totalBalance}
                 isSelectable={account.accountIndex !== disabledJar}
                 isSelected={account.accountIndex === selectedJar}
-                fill={calculateFillLevel(account.totalBalance, totalBalance)}
+                fillLevel={calculateFillLevel(account.totalBalance, totalBalance)}
                 onClick={() => setSelectedJar(account.accountIndex)}
               />
             )
           })}
         </div>
       </rb.Modal.Body>
-      <rb.Modal.Footer className={styles['modal-footer']}>
+      <rb.Modal.Footer className={styles.modalFooter}>
         <rb.Button variant="light" onClick={cancel} className="d-flex justify-content-center align-items-center">
-          Cancel
+          {t('modal.confirm_button_reject')}
         </rb.Button>
-        <rb.Button variant="dark" onClick={confirm}>
-          Confirm
+        <rb.Button disabled={selectedJar === null} variant="dark" onClick={confirm}>
+          {t('modal.confirm_button_accept')}
         </rb.Button>
       </rb.Modal.Footer>
     </rb.Modal>
--- src/components/Jars.jsx
+++ src/components/Jars.jsx
@@ -1,65 +1,9 @@
-import React, { useState, useRef, useMemo } from 'react'
+import React, { useMemo } from 'react'
 import * as rb from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
-import { useSettings } from '../context/SettingsContext'
 import styles from './Jars.module.css'
 import Sprite from './Sprite'
-import Balance from './Balance'
-
-const Jar = ({ accountIndex, balance, fill, onClick }) => {
-  const settings = useSettings()
-  const { t } = useTranslation()
-
-  const [jarIsOpen, setJarIsOpen] = useState(false)
-  const tooltipTarget = useRef(null)
-
-  const jarSymbol = ((fill) => {
-    switch (fill) {
-      case 1:
-        return jarIsOpen ? 'jar-open-fill-25' : 'jar-closed-fill-25'
-      case 2:
-        return jarIsOpen ? 'jar-open-fill-50' : 'jar-closed-fill-50'
-      case 3:
-        return jarIsOpen ? 'jar-open-fill-75' : 'jar-closed-fill-75'
-      default:
-        return jarIsOpen ? 'jar-open-empty' : 'jar-closed-empty'
-    }
-  })(fill)
-
-  const onMouseOver = () => setJarIsOpen(true)
-  const onMouseOut = () => setJarIsOpen(false)
-
-  return (
-    <div ref={tooltipTarget} className={styles['jar-container']} onMouseOver={onMouseOver} onMouseOut={onMouseOut}>
-      <rb.Overlay
-        target={tooltipTarget.current}
-        show={jarIsOpen}
-        placement="top"
-        popperConfig={{
-          modifiers: [
-            {
-              name: 'offset',
-              options: {
-                offset: [0, 5],
-              },
-            },
-          ],
-        }}
-      >
-        {(props) => (
-          <rb.Tooltip id="jar-tooltip" {...props}>
-            {t('current_wallet.jar_tooltip')}
-          </rb.Tooltip>
-        )}
-      </rb.Overlay>
-      <Sprite className={styles['jar-sprite']} symbol={jarSymbol} width="32px" height="48px" onClick={onClick} />
-      <div className={styles['jar-index']}>{'#' + accountIndex}</div>
-      <div className={styles['jar-balance']}>
-        <Balance valueString={balance} convertToUnit={settings.unit} showBalance={settings.showBalance} />
-      </div>
-    </div>
-  )
-}
+import { calculateFillLevel, OpenableJar } from './jars/Jar'
 
 const Jars = ({ accountBalances, totalBalance, onClick }) => {
   const { t } = useTranslation()
@@ -68,43 +12,29 @@ const Jars = ({ accountBalances, totalBalance, onClick }) => {
     return Object.values(accountBalances).sort((lhs, rhs) => lhs.accountIndex - rhs.accountIndex)
   }, [accountBalances])
 
-  // Classifies the account balance into one of four groups:
-  // - More than half of the total balance
-  // - More than a quarter of the total balance
-  // - Not empty
-  // - Empty
-  const calculateFillLevel = (accountBalance, totalBalance) => {
-    if (!totalBalance) return 0
-
-    if (accountBalance > totalBalance / 2) return 3
-    if (accountBalance > totalBalance / 4) return 2
-    if (accountBalance > 0) return 1
-
-    return 0
-  }
-
   const jarsDescriptionPopover = (
-    <rb.Popover id="popover-basic">
+    <rb.Popover>
       <rb.Popover.Body>{t('current_wallet.jars_title_popover')}</rb.Popover.Body>
     </rb.Popover>
   )
 
   return (
     <div className="d-flex flex-column align-items-center gap-5">
-      <rb.OverlayTrigger placement="top" overlay={jarsDescriptionPopover}>
-        <div className={styles['jars-title']}>
+      <rb.OverlayTrigger placement="right" overlay={jarsDescriptionPopover}>
+        <div className={styles.jarsTitle}>
           <div>{t('current_wallet.jars_title')}</div>
-          <Sprite className={styles['info-icon']} symbol="info" width="18" height="18" />
+          <Sprite className={styles.infoIcon} symbol="info" width="18" height="18" />
         </div>
       </rb.OverlayTrigger>
-      <div className={styles['jars-container']}>
+      <div className={styles.jarsContainer}>
         {sortedAccountBalances.map((account) => {
           return (
-            <Jar
+            <OpenableJar
               key={account.accountIndex}
-              accountIndex={account.accountIndex}
+              index={account.accountIndex}
               balance={account.totalBalance}
-              fill={calculateFillLevel(account.totalBalance, totalBalance)}
+              fillLevel={calculateFillLevel(account.totalBalance, totalBalance)}
+              tooltipText={t('current_wallet.jar_tooltip')}
               onClick={() => onClick(account.accountIndex)}
             />
           )
