669d2362c143210c031139705a0d2f8bc3d48a76
Daniel <10026790+dnlggr@users.noreply.github.com>
2022-06-22 14:30:12 +0200

    fix: remove unused import

--- src/components/jars/Jar.tsx
+++ src/components/jars/Jar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, useMemo } from 'react'
+import React, { useState, useRef, useMemo } from 'react'
 import * as rb from 'react-bootstrap'
 import classnames from 'classnames/bind'
 import styles from './Jar.module.css'
