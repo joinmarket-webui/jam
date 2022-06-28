eb82a0bb06522a44069795e088995a9630a1fabb
theborakompanioni <theborakompanioni+github@gmail.com>
2022-06-28 20:08:26 +0200

    refactor: ConfirmModal from jsx to tsx
    also use 'children' child node instead of 'body' property

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { PropsWithChildren } from 'react'
 import * as rb from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
 import styles from './Modal.module.css'
 import Sprite from './Sprite'
 
-const ConfirmModal = ({ isShown, title, body, onCancel, onConfirm }) => {
+interface ConfirmModalProps {
+  isShown: boolean
+  title: React.ReactNode | string
+  onCancel: () => void
+  onConfirm: () => void
+}
+
+const ConfirmModal = ({ isShown, title, children, onCancel, onConfirm }: PropsWithChildren<ConfirmModalProps>) => {
   const { t } = useTranslation()
 
   return (
     <rb.Modal
       show={isShown}
       keyboard={true}
-      onEscapeKeyDown={onCancel}
+      onEscapeKeyDown={() => onCancel()}
       centered={true}
       animation={true}
       backdrop="static"
@@ -20,13 +27,13 @@ const ConfirmModal = ({ isShown, title, body, onCancel, onConfirm }) => {
       <rb.Modal.Header className={styles['modal-header']}>
         <rb.Modal.Title className={styles['modal-title']}>{title}</rb.Modal.Title>
       </rb.Modal.Header>
-      <rb.Modal.Body className={styles['modal-body']}>{body}</rb.Modal.Body>
+      <rb.Modal.Body className={styles['modal-body']}>{children}</rb.Modal.Body>
       <rb.Modal.Footer className={styles['modal-footer']}>
         <rb.Button variant="light" onClick={onCancel} className="d-flex justify-content-center align-items-center">
           <Sprite symbol="cancel" width="26" height="26" />
           <div>{t('modal.confirm_button_reject')}</div>
         </rb.Button>
-        <rb.Button variant="dark" onClick={onConfirm}>
+        <rb.Button variant="dark" onClick={() => onConfirm()}>
           {t('modal.confirm_button_accept')}
         </rb.Button>
       </rb.Modal.Footer>
