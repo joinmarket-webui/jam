15a706683c4d57ed811bf48b7e58796e6970c31b
Daniel <10026790+dnlggr@users.noreply.github.com>
2022-07-27 15:53:34 +0200

    refactor: clean up table

--- src/components/jar_details/JarDetailsOverlay.tsx
+++ src/components/jar_details/JarDetailsOverlay.tsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect, useMemo, useCallback } from 'react'
 import * as rb from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
-import { Account, Utxo, WalletInfo, CurrentWallet } from '../../context/WalletContext'
+import * as Api from '../../libs/JmWalletApi'
+import { Account, Utxo, WalletInfo, CurrentWallet, useReloadCurrentWalletInfo } from '../../context/WalletContext'
 import Sprite from '../Sprite'
 import SegmentedTabs from '../SegmentedTabs'
+import { UtxoList } from './UtxoList'
 import styles from './JarDetailsOverlay.module.css'
 
 const TABS = {
@@ -39,9 +41,9 @@ const Header = ({ account, nextAccount, previousAccount, setTab, onHide }: Heade
 
   return (
     <>
-      <div className="w-100 d-flex gap-3">
+      <div className="w-100 d-flex flex-column flex-md-row gap-3">
         <div className="d-flex align-items-center flex-1">
-          <rb.Button variant="link" className="unstyled ps-0 me-auto" onClick={onHide}>
+          <rb.Button variant="link" className="unstyled ps-0 ms-auto mx-md-0 me-auto" onClick={onHide}>
             <span>{t('global.close')}</span>
           </rb.Button>
         </div>
@@ -54,7 +56,7 @@ const Header = ({ account, nextAccount, previousAccount, setTab, onHide }: Heade
           />
         </div>
         <div className="d-flex flex-1">
-          <div className="d-flex align-items-center ms-auto">
+          <div className="d-flex align-items-center ms-auto me-auto me-md-0">
             <rb.Button variant="link" className={styles.accountStepperButton} onClick={() => previousAccount()}>
               <Sprite symbol="caret-left" width="20" height="20" />
             </rb.Button>
@@ -73,8 +75,13 @@ const Header = ({ account, nextAccount, previousAccount, setTab, onHide }: Heade
 }
 
 const JarDetailsOverlay = (props: JarDetailsOverlayProps) => {
+  const reloadCurrentWalletInfo = useReloadCurrentWalletInfo()
+
   const [accountIndex, setAccountIndex] = useState(props.initialAccountIndex)
   const [selectedTab, setSelectedTab] = useState(TABS.UTXOS)
+  const [isLoadingFreeze, setIsLoadingFreeze] = useState(false)
+  const [isLoadingUnfreeze, setIsLoadingUnfreeze] = useState(false)
+  const [selectedUtxoIds, setSelectedUtxoIds] = useState<Array<string>>([])
 
   const account = useMemo(() => props.accounts[accountIndex], [props.accounts, accountIndex])
 
@@ -102,6 +109,71 @@ const JarDetailsOverlay = (props: JarDetailsOverlayProps) => {
     return () => document.removeEventListener('keydown', onKeyDown)
   }, [props.isShown, onKeyDown])
 
+  const changeSelectedUtxoFreeze = async (freeze: boolean) => {
+    if (selectedUtxoIds.length <= 0) return
+
+    const selectedUtxos = (props.utxosByAccount[accountIndex] || []).filter((utxo: Utxo) =>
+      selectedUtxoIds.includes(utxo.utxo)
+    )
+
+    freeze ? setIsLoadingFreeze(true) : setIsLoadingUnfreeze(true)
+
+    const abortCtrl = new AbortController()
+
+    const { name: walletName, token } = props.wallet
+
+    const freezeCalls = selectedUtxos.map((utxo) =>
+      Api.postFreeze({ walletName, token }, { utxo: utxo.utxo, freeze: freeze }).then((res) => {
+        if (!res.ok) {
+          return Api.Helper.throwError(res, freeze ? 'error_freezing_utxos' : 'error_unfreezing_utxos')
+        }
+      })
+    )
+
+    Promise.all(freezeCalls)
+      .then((_) => reloadCurrentWalletInfo({ signal: abortCtrl.signal }))
+      .catch((err) => {
+        // setAlert({ variant: 'danger', message: err.message, dismissible: true })
+      })
+      .finally(() => {
+        freeze ? setIsLoadingFreeze(false) : setIsLoadingUnfreeze(false)
+      })
+  }
+
+  const utxoListTitle = () => {
+    const utxos = props.utxosByAccount[accountIndex] || []
+
+    if (utxos.length === 0) return `No UTXOs in Jar #${accountIndex}`
+    if (utxos.length === 1) return `1 UTXO in Jar #${accountIndex}`
+
+    return `${utxos.length} UTXOs in Jar #${accountIndex}`
+  }
+
+  const freezeUnfreezeButton = ({ freeze }: { freeze: boolean }) => {
+    const isLoading = freeze ? isLoadingFreeze : isLoadingUnfreeze
+    return (
+      <rb.Button
+        disabled={freeze ? isLoadingUnfreeze : isLoadingFreeze}
+        variant="light"
+        onClick={() => {
+          changeSelectedUtxoFreeze(freeze)
+        }}
+      >
+        {isLoading ? (
+          <>
+            <rb.Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" className="me-1" />
+            <div>{freeze ? 'Freezing...' : 'Unfreezing...'}</div>
+          </>
+        ) : (
+          <>
+            <Sprite symbol="snowflake" width="16" height="16" />
+            <div>{freeze ? 'Freeze selected' : 'Unfreeze selected'}</div>
+          </>
+        )}
+      </rb.Button>
+    )
+  }
+
   return (
     <rb.Offcanvas
       className={styles.overlayContainer}
@@ -124,7 +196,28 @@ const JarDetailsOverlay = (props: JarDetailsOverlayProps) => {
       <rb.Offcanvas.Body className={styles.overlayBody}>
         <rb.Container className="py-4 py-sm-5">
           <rb.Row className="justify-content-center">
-            <rb.Col lg={12} xl={10}></rb.Col>
+            <rb.Col>
+              <div className={styles.utxoListContainer}>
+                <div className={styles.utxoListTitleBar}>
+                  <div>{utxoListTitle()}</div>
+                  {(props.utxosByAccount[accountIndex] || []).length > 0 && (
+                    <div className={styles.freezeUnfreezeButtonsContainer}>
+                      {freezeUnfreezeButton({ freeze: true })}
+                      {freezeUnfreezeButton({ freeze: false })}
+                    </div>
+                  )}
+                </div>
+                {(props.utxosByAccount[accountIndex] || []).length > 0 && (
+                  <div className="px-md-3">
+                    <UtxoList
+                      utxos={props.utxosByAccount[accountIndex] || []}
+                      walletInfo={props.walletInfo}
+                      setSelectedUtxoIds={setSelectedUtxoIds}
+                    />
+                  </div>
+                )}
+              </div>
+            </rb.Col>
           </rb.Row>
         </rb.Container>
       </rb.Offcanvas.Body>
---
+++ src/components/jar_details/UtxoList.tsx
@@ -0,0 +1,240 @@
+import React, { useEffect, useMemo } from 'react'
+import * as rb from 'react-bootstrap'
+import classnames from 'classnames'
+import { Table, Header, HeaderRow, HeaderCell, Body, Row, Cell } from '@table-library/react-table-library/table'
+import { useRowSelect, HeaderCellSelect, CellSelect, SelectTypes } from '@table-library/react-table-library/select'
+import { useSort, HeaderCellSort } from '@table-library/react-table-library/sort'
+import * as TableTypes from '@table-library/react-table-library/types/table'
+import { useTheme } from '@table-library/react-table-library/theme'
+import { useSettings } from '../../context/SettingsContext'
+import { Utxo, WalletInfo } from '../../context/WalletContext'
+import * as fb from '../fb/utils'
+import Sprite from '../Sprite'
+import Balance from '../Balance'
+import styles from './UtxoList.module.css'
+
+const cx = classnames.bind(styles)
+
+const ADDRESS_STATUS_COLORS: { [key: string]: string } = {
+  new: 'normal',
+  used: 'normal',
+  reused: 'danger',
+  'cj-out': 'success',
+  'change-out': 'warning',
+  'non-cj-change': 'normal',
+  deposit: 'normal',
+}
+
+const SORT_KEYS = {
+  frozenOrLocked: 'FROZEN_OR_LOCKED',
+  value: 'VALUE',
+}
+
+const TABLE_THEME = {
+  Table: `
+    --data-table-library_grid-template-columns: 2rem 3.5rem 2fr 4fr 3fr 1fr;
+    font-size: 0.9rem;
+  `,
+  BaseCell: `
+    &:nth-of-type(1) {
+      text-align: center;
+    }
+  `,
+  Cell: `
+    &:nth-of-type(2) {
+      text-align: center;
+    }
+  `,
+  Row: `
+    &.row-select-selected {
+      color: inherit;
+      font-weight: inherit;
+    }
+  `,
+}
+
+interface UtxoListProps {
+  utxos: Array<Utxo>
+  walletInfo: WalletInfo
+  setSelectedUtxoIds: (selectedUtxoIds: Array<string>) => void
+}
+
+const UtxoList = ({ utxos, walletInfo, setSelectedUtxoIds }: UtxoListProps) => {
+  const settings = useSettings()
+
+  const toUtxo = (tableNode: TableTypes.TableNode): Utxo => {
+    const { id, ...utxo } = tableNode
+
+    return utxo as Utxo
+  }
+
+  const utxoTags = (utxo: Utxo, walletInfo: WalletInfo) => {
+    var rawStatus = walletInfo.addressSummary[utxo.address]?.status
+
+    var status: string | null = null
+    var locktime: string | null = null
+
+    if (rawStatus) {
+      const indexOfLockedTag = rawStatus.indexOf('[LOCKED]')
+
+      if (indexOfLockedTag !== -1) {
+        locktime = rawStatus.substring(0, indexOfLockedTag).trim()
+      } else {
+        const indexOfOtherTag = rawStatus.indexOf('[')
+
+        if (indexOfOtherTag !== -1) {
+          status = rawStatus.substring(0, indexOfOtherTag).trim()
+        } else {
+          status = rawStatus
+        }
+      }
+    }
+
+    let tags = []
+
+    if (utxo.frozen) tags.push({ tag: 'frozen', color: 'normal' })
+    if (utxo.label) tags.push({ tag: utxo.label, color: 'normal' })
+    if (status) tags.push({ tag: status, color: ADDRESS_STATUS_COLORS[status] || 'normal' })
+    if (fb.utxo.isLocked(utxo) && locktime) tags.push({ tag: `locked until ${locktime}`, color: 'normal' })
+
+    return tags
+  }
+
+  const tableData: TableTypes.Data = useMemo(
+    () => ({
+      nodes: utxos.map((utxo: Utxo) => ({ ...utxo, id: utxo.utxo })),
+    }),
+    [utxos]
+  )
+
+  const tableTheme = useTheme(TABLE_THEME)
+
+  const onTableSelectChange = (action: any, state: any) => {
+    setSelectedUtxoIds(state.ids)
+  }
+
+  const tableSelect = useRowSelect(
+    tableData,
+    {
+      onChange: onTableSelectChange,
+    },
+    {
+      rowSelect: SelectTypes.MultiSelect,
+      buttonSelect: SelectTypes.MultiSelect,
+    }
+  )
+
+  const tableSort = useSort(
+    tableData,
+    {
+      state: {
+        sortKey: SORT_KEYS.value,
+        reverse: true,
+      },
+    },
+    {
+      sortIcon: {
+        margin: '4px',
+        iconDefault: <Sprite symbol="caret-right" width="20" height="20" />,
+        iconUp: <Sprite symbol="caret-up" width="20" height="20" />,
+        iconDown: <Sprite symbol="caret-down" width="20" height="20" />,
+      },
+      sortFns: {
+        [SORT_KEYS.frozenOrLocked]: (array) =>
+          array.sort((a, b) => {
+            const aUtxo = toUtxo(a)
+            const bUtxo = toUtxo(b)
+            if (fb.utxo.isLocked(aUtxo) && !fb.utxo.isLocked(bUtxo)) return -1
+            if (!fb.utxo.isLocked(aUtxo) && fb.utxo.isLocked(bUtxo)) return 1
+            if (fb.utxo.isLocked(aUtxo) && fb.utxo.isLocked(bUtxo)) return 0
+            if (aUtxo.frozen && !bUtxo.frozen) return -1
+            if (!aUtxo.frozen && bUtxo.frozen) return 1
+            if (aUtxo.frozen && bUtxo.frozen) return 0
+            return 0
+          }),
+        [SORT_KEYS.value]: (array) => array.sort((a, b) => a.value - b.value),
+      },
+    }
+  )
+
+  const frozenOrLocked = (utxo: Utxo) => {
+    if (utxo.frozen && !fb.utxo.isLocked(utxo)) {
+      return (
+        <div className={styles.frozenLockedTag}>
+          <Sprite className={styles.iconFrozen} symbol="snowflake" width="20" height="20" />
+        </div>
+      )
+    }
+    if (fb.utxo.isLocked(utxo)) {
+      return (
+        <div className={styles.frozenLockedTag}>
+          <Sprite className={styles.iconLocked} symbol="timelock" width="20" height="20" />
+        </div>
+      )
+    }
+  }
+
+  return (
+    <div className={styles.utxoList}>
+      <Table
+        data={tableData}
+        theme={tableTheme}
+        select={tableSelect}
+        sort={tableSort}
+        layout={{ custom: true, horizontalScroll: true }}
+      >
+        {(tableList) => (
+          <>
+            <Header>
+              <HeaderRow>
+                <HeaderCellSelect />
+                <HeaderCellSort sortKey={SORT_KEYS.frozenOrLocked}>
+                  <Sprite symbol="coins" width="20" height="20" className={styles.headerCoinsIcon} />
+                </HeaderCellSort>
+                <HeaderCellSort sortKey={SORT_KEYS.value}>Value</HeaderCellSort>
+                <HeaderCell>Address</HeaderCell>
+                <HeaderCell>Tags</HeaderCell>
+                <HeaderCell></HeaderCell>
+              </HeaderRow>
+            </Header>
+            <Body>
+              {tableList.map((item) => (
+                <Row key={item.id} item={item}>
+                  <CellSelect item={item} />
+                  <Cell>{frozenOrLocked(toUtxo(item))}</Cell>
+                  <Cell>
+                    <Balance
+                      valueString={toUtxo(item).value.toString()}
+                      convertToUnit={settings.unit}
+                      showBalance={settings.showBalance}
+                    />
+                  </Cell>
+                  <Cell>
+                    <code>{toUtxo(item).address}</code>
+                  </Cell>
+                  <Cell>
+                    <div className={styles.utxoTagList}>
+                      {utxoTags(toUtxo(item), walletInfo).map((tag, index) => (
+                        <div className={classnames(styles.utxoTag, styles[`utxoTag-${tag.color}`])}>
+                          <div />
+                          <div>{tag.tag}</div>
+                        </div>
+                      ))}
+                    </div>
+                  </Cell>
+                  <Cell>
+                    <rb.Button className={styles.utxoListButtonDetails} variant="link" onClick={() => {}}>
+                      Details
+                    </rb.Button>
+                  </Cell>
+                </Row>
+              ))}
+            </Body>
+          </>
+        )}
+      </Table>
+    </div>
+  )
+}
+
+export { UtxoList }
