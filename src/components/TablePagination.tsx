0c21d07d483daa264427fcc1262a72fc28de5ff3
theborakompanioni <theborakompanioni+github@gmail.com>
2022-08-05 21:04:44 +0200

    dev: add TablePagination component

---
+++ src/components/TablePagination.tsx
@@ -0,0 +1,102 @@
+import React from 'react'
+import { Pagination } from '@table-library/react-table-library/pagination'
+import * as TableTypes from '@table-library/react-table-library/types/table'
+import * as rb from 'react-bootstrap'
+import { useTranslation } from 'react-i18next'
+
+const DEFAULT_PAGE_SIZES = [25, 50, 100]
+const DEFAULT_ALLOW_SHOW_ALL = true
+
+interface TablePaginationProps {
+  data: TableTypes.Data
+  pagination: Pagination
+  pageSizes?: number[]
+  allowShowAll?: boolean
+}
+
+export default function TablePagination({
+  data,
+  pagination,
+  pageSizes = DEFAULT_PAGE_SIZES,
+  allowShowAll = DEFAULT_ALLOW_SHOW_ALL,
+}: TablePaginationProps) {
+  const { t } = useTranslation()
+
+  return (
+    <div className="d-flex justify-content-between flex-column flex-md-row">
+      <div className="mt-3 mt-md-0 ms-3 ms-md-0 d-flex justify-content-center align-items-center order-2 order-md-1">
+        {t('global.table.pagination.items_per_page.label')}
+        <rb.Form.Select
+          aria-label={t('global.table.pagination.items_per_page.label')}
+          className="ms-2 d-inline-block w-auto"
+          defaultValue={pageSizes[0]}
+          onChange={(e) => {
+            const value = parseInt(e.target.value, 10)
+            const pageSize = value > 0 ? value : data.nodes.length
+            pagination.fns.onSetSize(pageSize)
+          }}
+        >
+          {pageSizes.map((size) => (
+            <option key={size} value={size}>
+              {size}
+            </option>
+          ))}
+          {allowShowAll && (
+            <option value={-1}>{t('global.table.pagination.items_per_page.text_option_show_all')}</option>
+          )}
+        </rb.Form.Select>
+      </div>
+
+      <div className="mt-3 mt-md-0 ms-3 ms-md-0 d-flex justify-content-center align-items-center order-1 order-md-2">
+        <rb.Button
+          variant={'outline-dark'}
+          className="ms-1"
+          disabled={pagination.state.page === 0}
+          onClick={() => pagination.fns.onSetPage(0)}
+        >
+          {t('global.table.pagination.page_selector.label_first')}
+        </rb.Button>
+        <rb.Button
+          variant="outline-dark"
+          className="ms-1"
+          disabled={pagination.state.page === 0}
+          onClick={() => pagination.fns.onSetPage(pagination.state.page - 1)}
+        >
+          {t('global.table.pagination.page_selector.label_previous')}
+        </rb.Button>
+        <rb.Form.Select
+          aria-label={t('global.table.pagination.page_selector.label')}
+          className="ms-1 d-inline-block w-auto h-auto"
+          value={pagination.state.page}
+          onChange={(e) => {
+            const value = parseInt(e.target.value, 10)
+            const page = value > 0 ? value : 0
+            pagination.fns.onSetPage(page)
+          }}
+        >
+          {pagination.state.getPages(data.nodes).map((_: any, index: number) => (
+            <option key={index} value={index}>
+              {index + 1}
+            </option>
+          ))}
+        </rb.Form.Select>
+        <rb.Button
+          variant="outline-dark"
+          className="ms-1"
+          disabled={pagination.state.page + 1 === pagination.state.getTotalPages(data.nodes)}
+          onClick={() => pagination.fns.onSetPage(pagination.state.page + 1)}
+        >
+          {t('global.table.pagination.page_selector.label_next')}
+        </rb.Button>
+        <rb.Button
+          variant="outline-dark"
+          className="ms-1"
+          disabled={pagination.state.page + 1 === pagination.state.getTotalPages(data.nodes)}
+          onClick={() => pagination.fns.onSetPage(pagination.state.getTotalPages(data.nodes) - 1)}
+        >
+          {t('global.table.pagination.page_selector.label_last')}
+        </rb.Button>
+      </div>
+    </div>
+  )
+}
