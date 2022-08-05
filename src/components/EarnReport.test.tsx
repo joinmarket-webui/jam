b8cd6d9b19f1c120ea83cb221a895f5fd01803d1
theborakompanioni <theborakompanioni+github@gmail.com>
2022-08-05 13:29:31 +0200

    test: show handling unexpected/malformed data in Earn Report

--- src/components/EarnReport.test.tsx
+++ src/components/EarnReport.test.tsx
@@ -1,5 +1,8 @@
 import { yieldgenReportToEarnReportEntries } from './EarnReport'
 
+const EXPECTED_HEADER_LINE =
+  'timestamp,cj amount/satoshi,my input count,my input value/satoshi,cjfee/satoshi,earned/satoshi,confirm time/min,notes\n'
+
 describe('Earn Report', () => {
   it('should parse empty data correctly', () => {
     const entries = yieldgenReportToEarnReportEntries([])
@@ -7,20 +10,16 @@ describe('Earn Report', () => {
   })
 
   it('should parse data only containing headers correctly', () => {
-    const onlyHeaders = [
-      'timestamp,cj amount/satoshi,my input count,my input value/satoshi,cjfee/satoshi,earned/satoshi,confirm time/min,notes\n',
-    ]
-
-    const entries = yieldgenReportToEarnReportEntries(onlyHeaders)
+    const entries = yieldgenReportToEarnReportEntries([EXPECTED_HEADER_LINE])
 
     expect(entries.length).toBe(0)
   })
 
   it('should parse expected data structure correctly', () => {
     const exampleData = [
-      'timestamp,cj amount/satoshi,my input count,my input value/satoshi,cjfee/satoshi,earned/satoshi,confirm time/min,notes\n',
-      '2009/01/03 02:54:42,,,,,,,Connected\n',
-      '2009/01/03 03:02:12,14999989490,4,20000005630,250,250,0.42,\n',
+      EXPECTED_HEADER_LINE,
+      '2008/10/31 02:42:54,,,,,,,Connected\n',
+      '2009/01/03 02:54:42,14999989490,4,20000005630,250,250,0.42,\n',
       '2009/01/03 03:03:32,10000000000,3,15000016390,250,250,0.8,\n',
       '2009/01/03 03:04:47,4999981140,1,5000016640,250,250,0,\n',
       '2009/01/03 03:06:07,1132600000,1,2500000000,250,250,13.37,\n',
@@ -33,7 +32,7 @@ describe('Earn Report', () => {
     expect(entries.length).toBe(7)
 
     const firstEntry = entries[0]
-    expect(firstEntry.timestamp.toUTCString()).toBe('Sat, 03 Jan 2009 02:54:42 GMT')
+    expect(firstEntry.timestamp.toUTCString()).toBe('Fri, 31 Oct 2008 02:42:54 GMT')
     expect(firstEntry.cjTotalAmount).toBe(null)
     expect(firstEntry.inputCount).toBe(null)
     expect(firstEntry.inputAmount).toBe(null)
@@ -51,5 +50,40 @@ describe('Earn Report', () => {
     expect(lastEntry.earnedAmount).toBe(250)
     expect(lastEntry.confirmationDuration).toBe(0.17)
     expect(lastEntry.notes).toBe('\n')
+  })
+
+  it('should handle unexpected/malformed data in a sane way', () => {
+    const unexpectedHeader = EXPECTED_HEADER_LINE + ',foo,bar'
+    const emptyLine = '' // should be skipped
+    const onlyNewLine = '\n' // should be skipped
+    const shortLine = '2009/01/03 04:04:04,,,' // should be skipped
+    const longLine = '2009/01/03 05:05:05,,,,,,,,,,,,,,,,,,,,,,,' // should be parsed
+    const malformedLine = 'this,is,a,malformed,line,with,some,unexpected,data' // should be parsed
+
+    const exampleData = [unexpectedHeader, emptyLine, onlyNewLine, shortLine, longLine, malformedLine]
+
+    const entries = yieldgenReportToEarnReportEntries(exampleData)
+
+    expect(entries.length).toBe(2)
+
+    const firstEntry = entries[0]
+    expect(firstEntry.timestamp.toUTCString()).toBe('Sat, 03 Jan 2009 05:05:05 GMT')
+    expect(firstEntry.cjTotalAmount).toBe(null)
+    expect(firstEntry.inputCount).toBe(null)
+    expect(firstEntry.inputAmount).toBe(null)
+    expect(firstEntry.fee).toBe(null)
+    expect(firstEntry.earnedAmount).toBe(null)
+    expect(firstEntry.confirmationDuration).toBe(null)
+    expect(firstEntry.notes).toBe(null)
+
+    const secondEntry = entries[1]
+    expect(secondEntry.timestamp.toUTCString()).toBe('Invalid Date')
+    expect(secondEntry.cjTotalAmount).toBe(NaN)
+    expect(secondEntry.inputCount).toBe(NaN)
+    expect(secondEntry.inputAmount).toBe(NaN)
+    expect(secondEntry.fee).toBe(NaN)
+    expect(secondEntry.earnedAmount).toBe(NaN)
+    expect(secondEntry.confirmationDuration).toBe(NaN)
+    expect(secondEntry.notes).toBe('unexpected')
   })
 })
