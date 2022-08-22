201a7e40e863c8180e4b345fc33abf1ddc748eeb
theborakompanioni <theborakompanioni+github@gmail.com>
2022-08-23 01:19:29 +0200

    dev: easier testing of log feature in dev env - part 2

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,65 +2,99 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 const { PUBLIC_URL = '' } = process.env
 
-const __TEST_LOG_FEATURE = true
-
-const SECONDARY_CONTAINER_JAM_API_PORT = 29080
-
 const PRIMARY_CONTAINER = {
   apiPort: 28183,
   websocketPort: 28283,
   obwatcherPort: 62601,
 }
-const SECONDARY_CONTAINER = {
-  apiPort: 29183,
-  websocketPort: 29283,
-  obwatcherPort: 72601,
-}
 
-let target = PRIMARY_CONTAINER
+const SECONDARY_CONTAINER_JAM_API_PORT = 29080
+
+const __TEST_LOG_FEATURE = false
+
+let target = 'PRIMARY'
 if (__TEST_LOG_FEATURE) {
-  target = SECONDARY_CONTAINER
+  target = 'SECONDARY'
 }
 
 module.exports = (app) => {
-  app.use(
-    createProxyMiddleware(`${PUBLIC_URL}/api/`, {
-      target: `https://localhost:${target.apiPort}`,
-      pathRewrite: { [`^${PUBLIC_URL}`]: '' },
-      changeOrigin: true,
-      secure: false,
-      onProxyReq: (proxyReq, req, res) => {
-        if (req.headers['x-jm-authorization']) {
-          proxyReq.setHeader('Authorization', req.headers['x-jm-authorization'])
-        }
-      },
-    })
-  )
-
-  // https://github.com/JoinMarket-Org/joinmarket-clientserver/blob/master/docs/JSON-RPC-API-using-jmwalletd.md#websocket
-  app.use(
-    createProxyMiddleware(`${PUBLIC_URL}/jmws`, {
-      target: `https://localhost:${target.websocketPort}`,
-      pathRewrite: { [`^${PUBLIC_URL}/jmws`]: '' },
-      changeOrigin: true,
-      secure: false,
-      ws: true,
-    })
-  )
-
-  app.use(
-    createProxyMiddleware(`${PUBLIC_URL}/obwatch/`, {
-      target: `https://localhost:${target.obwatcherPort}`,
-      pathRewrite: { [`^${PUBLIC_URL}/obwatch/`]: '' },
-      changeOrigin: true,
-      secure: false,
-    })
-  )
-
-  if (__TEST_LOG_FEATURE) {
+  if (target === 'PRIMARY') {
+    /**
+     * The primary container *does not run* nginx!
+     * Requests must be adapted:
+     * - remove path prefix "PUBLIC_URL" (if present)
+     * - proxy requests to correct target service
+     * - rewrite paths to match target service paths
+     * - translate header "x-jm-authorization" to "Authorization"
+     */
     app.use(
-      createProxyMiddleware(`${PUBLIC_URL}/jam/api/v0/`, {
+      createProxyMiddleware(`${PUBLIC_URL}/jmws`, {
+        target: `https://localhost:${PRIMARY_CONTAINER.websocketPort}`,
+        pathRewrite: { [`^${PUBLIC_URL}/jmws`]: '' },
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      })
+    )
+
+    app.use(
+      createProxyMiddleware(`${PUBLIC_URL}/api/`, {
+        target: `https://localhost:${PRIMARY_CONTAINER.apiPort}`,
+        pathRewrite: { [`^${PUBLIC_URL}`]: '' },
+        changeOrigin: true,
+        secure: false,
+        onProxyReq: (proxyReq, req, res) => {
+          if (req.headers['x-jm-authorization']) {
+            proxyReq.setHeader('Authorization', req.headers['x-jm-authorization'])
+          }
+        },
+      })
+    )
+
+    app.use(
+      createProxyMiddleware(`${PUBLIC_URL}/obwatch/`, {
+        target: `https://localhost:${PRIMARY_CONTAINER.obwatcherPort}`,
+        pathRewrite: { [`^${PUBLIC_URL}/obwatch/`]: '' },
+        changeOrigin: true,
+        secure: false,
+      })
+    )
+  } else if (target === 'SECONDARY') {
+    /**
+     * The secondary container has the nginx ("Jam API") running!
+     * Requests must be adapted:
+     * - remove path prefix "PUBLIC_URL" (if present)
+     * - proxy all API requests to "Jam API"
+     */
+    app.use(
+      createProxyMiddleware(`${PUBLIC_URL}/jmws`, {
         target: `http://localhost:${SECONDARY_CONTAINER_JAM_API_PORT}`,
+        pathRewrite: { [`^${PUBLIC_URL}`]: '' },
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      })
+    )
+    app.use(
+      createProxyMiddleware(`${PUBLIC_URL}/api/`, {
+        target: `http://localhost:${SECONDARY_CONTAINER_JAM_API_PORT}`,
+        pathRewrite: { [`^${PUBLIC_URL}`]: '' },
+        changeOrigin: true,
+        secure: false,
+      })
+    )
+    app.use(
+      createProxyMiddleware(`${PUBLIC_URL}/obwatch/`, {
+        target: `http://localhost:${SECONDARY_CONTAINER_JAM_API_PORT}`,
+        pathRewrite: { [`^${PUBLIC_URL}`]: '' },
+        changeOrigin: true,
+        secure: false,
+      })
+    )
+    app.use(
+      createProxyMiddleware(`${PUBLIC_URL}/jam/`, {
+        target: `http://localhost:${SECONDARY_CONTAINER_JAM_API_PORT}`,
+        pathRewrite: { [`^${PUBLIC_URL}`]: '' },
         changeOrigin: true,
         secure: false,
       })
