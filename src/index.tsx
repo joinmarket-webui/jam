7342e1080c9c0e5913caadf872b7a40ea01dbe3a
Thebora Kompanioni <theborakompanioni@users.noreply.github.com>
2022-09-21 11:07:31 +0200

    build(deps): remove web-vitals (#506)
    * chore: dependencies update
    
    * chore: update test dependencies
    
    * chore: update bootstrap from v5.2.0 to v5.2.1
    
    * chore: update qrcode from v1.5.0 to v1.5.1
    
    * chore: update react-i18next from v11.16.9 to v11.18.6
    
    * chore: remove dependency web-vitals

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,6 @@ import { ServiceInfoProvider } from './context/ServiceInfoContext'
 import { WalletProvider } from './context/WalletContext'
 import { ServiceConfigProvider } from './context/ServiceConfigContext'
 
-import reportWebVitals from './reportWebVitals'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import './i18n/config'
@@ -44,8 +43,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
