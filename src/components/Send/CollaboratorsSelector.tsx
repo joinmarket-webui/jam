c1ce0dc052ad421589134ba25e83823f423615e8
theborakompanioni <theborakompanioni+github@gmail.com>
2023-01-17 14:36:59 +0100

    fix(CollaboratorsSelector): prevent form inside form

--- src/components/Send/CollaboratorsSelector.tsx
+++ src/components/Send/CollaboratorsSelector.tsx
@@ -38,65 +38,62 @@ const CollaboratorsSelector = ({
   }
 
   return (
-    // @ts-ignore FIXME: "Property 'disabled' does not exist on type..."
-    <rb.Form noValidate className={styles.collaboratorsSelector} disabled={disabled}>
-      <rb.Form.Group>
-        <rb.Form.Label className="mb-0">{t('send.label_num_collaborators', { numCollaborators })}</rb.Form.Label>
-        <div className="mb-2">
-          <rb.Form.Text className="text-secondary">{t('send.description_num_collaborators')}</rb.Form.Text>
-        </div>
-        <div className="d-flex flex-row flex-wrap gap-2">
-          {defaultCollaboratorsSelection.map((number) => {
-            const isSelected = !usesCustomNumCollaborators && numCollaborators === number
-            return (
-              <rb.Button
-                key={number}
-                variant={settings.theme === 'light' ? 'white' : 'dark'}
-                className={classNames(styles.collaboratorsSelectorElement, 'border', 'border-1', {
-                  [styles.selected]: isSelected,
-                })}
-                onClick={() => {
-                  setUsesCustomNumCollaborators(false)
-                  setNumCollaborators(number)
-                }}
-                disabled={disabled}
-              >
-                {number}
-              </rb.Button>
-            )
+    <rb.Form.Group className={styles.collaboratorsSelector}>
+      <rb.Form.Label className="mb-0">{t('send.label_num_collaborators', { numCollaborators })}</rb.Form.Label>
+      <div className="mb-2">
+        <rb.Form.Text className="text-secondary">{t('send.description_num_collaborators')}</rb.Form.Text>
+      </div>
+      <div className="d-flex flex-row flex-wrap gap-2">
+        {defaultCollaboratorsSelection.map((number) => {
+          const isSelected = !usesCustomNumCollaborators && numCollaborators === number
+          return (
+            <rb.Button
+              key={number}
+              variant={settings.theme === 'light' ? 'white' : 'dark'}
+              className={classNames(styles.collaboratorsSelectorElement, 'border', 'border-1', {
+                [styles.selected]: isSelected,
+              })}
+              onClick={() => {
+                setUsesCustomNumCollaborators(false)
+                setNumCollaborators(number)
+              }}
+              disabled={disabled}
+            >
+              {number}
+            </rb.Button>
+          )
+        })}
+        <rb.Form.Control
+          type="number"
+          min={minNumCollaborators}
+          max={99}
+          isInvalid={!isValidNumCollaborators(numCollaborators, minNumCollaborators)}
+          placeholder={t('send.input_num_collaborators_placeholder')}
+          defaultValue=""
+          className={classNames(styles.collaboratorsSelectorElement, 'border', 'border-1', {
+            [styles.selected]: usesCustomNumCollaborators,
           })}
-          <rb.Form.Control
-            type="number"
-            min={minNumCollaborators}
-            max={99}
-            isInvalid={!isValidNumCollaborators(numCollaborators, minNumCollaborators)}
-            placeholder={t('send.input_num_collaborators_placeholder')}
-            defaultValue=""
-            className={classNames(styles.collaboratorsSelectorElement, 'border', 'border-1', {
-              [styles.selected]: usesCustomNumCollaborators,
-            })}
-            onChange={(e) => {
+          onChange={(e) => {
+            setUsesCustomNumCollaborators(true)
+            validateAndSetCustomNumCollaborators(e.target.value)
+          }}
+          onClick={(e) => {
+            // @ts-ignore - FIXME: "Property 'value' does not exist on type 'EventTarget'"
+            if (e.target.value !== '') {
               setUsesCustomNumCollaborators(true)
-              validateAndSetCustomNumCollaborators(e.target.value)
-            }}
-            onClick={(e) => {
               // @ts-ignore - FIXME: "Property 'value' does not exist on type 'EventTarget'"
-              if (e.target.value !== '') {
-                setUsesCustomNumCollaborators(true)
-                // @ts-ignore - FIXME: "Property 'value' does not exist on type 'EventTarget'"
-                validateAndSetCustomNumCollaborators(e.target.value)
-              }
-            }}
-            disabled={disabled}
-          />
-          {usesCustomNumCollaborators && (
-            <rb.Form.Control.Feedback type="invalid">
-              {t('send.error_invalid_num_collaborators', { minNumCollaborators, maxNumCollaborators: 99 })}
-            </rb.Form.Control.Feedback>
-          )}
-        </div>
-      </rb.Form.Group>
-    </rb.Form>
+              validateAndSetCustomNumCollaborators(e.target.value)
+            }
+          }}
+          disabled={disabled}
+        />
+        {usesCustomNumCollaborators && (
+          <rb.Form.Control.Feedback type="invalid">
+            {t('send.error_invalid_num_collaborators', { minNumCollaborators, maxNumCollaborators: 99 })}
+          </rb.Form.Control.Feedback>
+        )}
+      </div>
+    </rb.Form.Group>
   )
 }
 
