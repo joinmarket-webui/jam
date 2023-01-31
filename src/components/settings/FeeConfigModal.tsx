e91e2694aeb69f6a8bc9d1ac2b9c9b398e0572bf
theborakompanioni <theborakompanioni+github@gmail.com>
2023-01-31 12:28:28 +0100

    fix(fees): allow min tx fee of 1sat/vbyte

--- src/components/settings/FeeConfigModal.tsx
+++ src/components/settings/FeeConfigModal.tsx
@@ -24,7 +24,7 @@ const TX_FEES_BLOCKS_MAX = 1_000
  * See https://github.com/JoinMarket-Org/joinmarket-clientserver/issues/1360#issuecomment-1262295463
  * Last checked on 2022-10-06.
  */
-const TX_FEES_SATSPERKILOVBYTE_MIN: SatsPerKiloVByte = 3_000 // 3 sat/vbyte
+const TX_FEES_SATSPERKILOVBYTE_MIN: SatsPerKiloVByte = 1_000 // 1 sat/vbyte
 // 350 sats/vbyte - no enforcement by JM - this should be a "sane" max value (taken default value of "absurd_fee_per_kb")
 const TX_FEES_SATSPERKILOVBYTE_MAX: SatsPerKiloVByte = 350_000
 const TX_FEES_SATSPERKILOVBYTE_ADJUSTED_MIN = 1_001 // actual min of `tx_fees` if unit is sats/kilo-vbyte
