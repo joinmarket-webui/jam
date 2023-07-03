550a43588b1b003a52a4ed3ba00b4623b9e7dd1d
Overload3910 <135011493+Overload3910@users.noreply.github.com>
2023-07-03 13:09:09 +0200

    feat(i18n): add Chinese translation (zh-Hans and zh-Hant) (#628)
    * i18n: add Chinese translation (Simplified and Traditional)
    
    Imported Chinese Translations from Transifex

--- src/i18n/languages.js
+++ src/i18n/languages.js
@@ -1,6 +1,8 @@
 import en from './locales/en/translation.json'
 import fr from './locales/fr/translation.json'
 import pt_BR from './locales/pt_BR/translation.json'
+import zh_Hans from './locales/zh_Hans/translation.json'
+import zh_Hant from './locales/zh_Hant/translation.json'
 
 const languages = [
   {
@@ -17,6 +19,16 @@ const languages = [
     key: 'pt-BR',
     description: 'Português (Brasil)',
     translation: pt_BR,
+  },
+  {
+    key: 'zh-Hans',
+    description: '中国语文科 (简化字)',
+    translation: zh_Hans,
+  },
+  {
+    key: 'zh-Hant',
+    description: '繁中国语文科 (繁体字)',
+    translation: zh_Hant,
   },
 ]
 
