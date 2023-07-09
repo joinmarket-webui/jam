416290893fc1f29ec2cde990e8af2987d4d6781a
theborakompanioni <theborakompanioni+github@gmail.com>
2023-07-10 01:08:31 +0200

    wip(import): add blockheight input to rescan chain form

--- src/components/RescanChain.tsx
+++ src/components/RescanChain.tsx
@@ -1,11 +1,100 @@
 import { useState, useCallback } from 'react'
 import * as rb from 'react-bootstrap'
+import { Trans, useTranslation } from 'react-i18next'
+import { Formik, FormikErrors } from 'formik'
 import * as Api from '../libs/JmWalletApi'
-import { useTranslation } from 'react-i18next'
 import { useServiceInfo } from '../context/ServiceInfoContext'
 import PageTitle from './PageTitle'
+import Sprite from './Sprite'
 import { CurrentWallet } from '../context/WalletContext'
-import styles from './RescanChain.module.css'
+
+type RescanChainFormValues = {
+  blockheight: number
+  gaplimit: number
+}
+
+const initialRescanChainFormValues: RescanChainFormValues = {
+  blockheight: 0,
+  gaplimit: 6,
+}
+
+interface RescanChainFormProps {
+  onSubmit: (values: RescanChainFormValues) => Promise<void>
+}
+
+const RescanChainForm = ({ onSubmit }: RescanChainFormProps) => {
+  const { t, i18n } = useTranslation()
+
+  return (
+    <Formik
+      initialValues={initialRescanChainFormValues}
+      validate={(values) => {
+        const errors = {} as FormikErrors<RescanChainFormValues>
+        if (values.blockheight < 0) {
+          errors.blockheight = t('rescan_chain.feedback_invalid_blockheight', {
+            min: 0,
+          })
+        }
+        if (values.gaplimit < 0) {
+          errors.gaplimit = t('rescan_chain.feedback_invalid_gaplimit', {
+            min: 0,
+          })
+        }
+        return errors
+      }}
+      onSubmit={onSubmit}
+    >
+      {({ handleSubmit, handleBlur, handleChange, values, touched, errors, isSubmitting }) => (
+        <rb.Form onSubmit={handleSubmit} noValidate lang={i18n.resolvedLanguage || i18n.language}>
+          <rb.Form.Group controlId="blockheight" className="mb-4">
+            <rb.Form.Label>{t('rescan_chain.label_blockheight')}</rb.Form.Label>
+            <rb.InputGroup hasValidation>
+              <rb.InputGroup.Text id="blockheight-addon1">
+                <Sprite symbol="block" width="24" height="24" name="Block" />
+              </rb.InputGroup.Text>
+              <rb.Form.Control
+                aria-label={t('rescan_chain.label_blockheight')}
+                className="slashed-zeroes"
+                name="blockheight"
+                type="number"
+                placeholder="1"
+                size="lg"
+                value={values.blockheight}
+                disabled={isSubmitting}
+                onBlur={handleBlur}
+                onChange={handleChange}
+                isValid={touched.blockheight && !errors.blockheight}
+                isInvalid={touched.blockheight && !!errors.blockheight}
+                min="0"
+                step="1"
+              />
+              <rb.Form.Control.Feedback type="invalid">{errors.blockheight}</rb.Form.Control.Feedback>
+            </rb.InputGroup>
+          </rb.Form.Group>
+          <rb.Button className="w-100" variant="dark" size="lg" type="submit" disabled={isSubmitting}>
+            <div className="d-flex justify-content-center align-items-center">
+              {isSubmitting ? (
+                <>
+                  <rb.Spinner
+                    as="span"
+                    animation="border"
+                    size="sm"
+                    role="status"
+                    aria-hidden="true"
+                    className="me-2"
+                  />
+                  {t('rescan_chain.text_button_submitting')}
+                </>
+              ) : (
+                <>{t('rescan_chain.text_button_submit')}</>
+              )}
+            </div>
+          </rb.Button>
+        </rb.Form>
+      )}
+    </Formik>
+  )
+}
 
 interface RescanChainProps {
   wallet: CurrentWallet
@@ -42,37 +131,31 @@ export default function RescanChain({ wallet }: RescanChainProps) {
 
   return (
     <div className="import-wallet">
-      <PageTitle title={t('rescan_chain.title')} subtitle={t('mnemonic_phrase.subtitle')} />
+      <PageTitle title={t('rescan_chain.title')} subtitle={t('rescan_chain.subtitle')} />
       {alert && <rb.Alert variant={alert.variant}>{alert.message}</rb.Alert>}
       <>
-        <>Your wallet has been imported.</>
-        <>In order for it to find existing funds, you need to rescan the blockchain.</>
-
-        <div>
+        <div className="mb-4">
+          <Trans i18nKey="rescan_chain.description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </Trans>
+        </div>
+        <div className="mb-4">
           {serviceInfo?.rescanning ? (
             <>Rescan in progress...</>
           ) : (
-            <>
-              <rb.Button
-                className={styles.submit}
-                variant="dark"
-                type="submit"
-                onClick={() => {
-                  const abortCtrl = new AbortController()
+            <RescanChainForm
+              onSubmit={async (values) => {
+                const abortCtrl = new AbortController()
 
-                  startChainRescan(abortCtrl.signal, {
-                    blockheight: 0,
-                  })
-                }}
-                disabled={isStartRescanning}
-              >
-                {isStartRescanning ? (
-                  <>{t('rescan_chain.text_button_submitting')}</>
-                ) : (
-                  <>{t('rescan_chain.text_button_submit')}</>
-                )}
-              </rb.Button>
-            </>
+                return startChainRescan(abortCtrl.signal, {
+                  blockheight: values.blockheight,
+                })
+              }}
+            />
           )}
         </div>
       </>
