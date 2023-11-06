0f3ec21c8432c9a9b096245181110256aa8d7280
theborakompanioni <theborakompanioni+github@gmail.com>
2023-11-06 13:25:08 +0100

    chore(balance): ability to specify space char

--- src/format.tsx
+++ src/format.tsx
@@ -1,30 +1,36 @@
 const BTC_FORMATTER = new Intl.NumberFormat('en-US', {
   minimumIntegerDigits: 1,
   minimumFractionDigits: 8,
+  maximumFractionDigits: 8,
 })
 
 const SATS_FORMATTER = new Intl.NumberFormat('en-US', {
   minimumIntegerDigits: 1,
   minimumFractionDigits: 0,
+  maximumFractionDigits: 0,
 })
 
-const _decimalPoint = '\u002E'
-const _thinSpace = '\u2009'
+const DECIMAL_POINT_CHAR = '\u002E'
 
 export type FormatBtcProps = {
+  spaceChar?: string
   deemphasize?: (char: string) => string | JSX.Element
 }
 
 const DEFAULT_FORMAT_BTC_PROPS: FormatBtcProps = {
+  spaceChar: '\u2009',
   deemphasize: (char) => <span className="opacity-50">{char}</span>,
 }
 
 export const formatBtc = (
   value: number,
-  { deemphasize = DEFAULT_FORMAT_BTC_PROPS.deemphasize! }: FormatBtcProps = DEFAULT_FORMAT_BTC_PROPS,
+  {
+    spaceChar = DEFAULT_FORMAT_BTC_PROPS.spaceChar!,
+    deemphasize = DEFAULT_FORMAT_BTC_PROPS.deemphasize!,
+  }: Partial<FormatBtcProps> = DEFAULT_FORMAT_BTC_PROPS,
 ) => {
   const numberString = BTC_FORMATTER.format(value)
-  const [integerPart, fractionalPart] = numberString.split(_decimalPoint)
+  const [integerPart, fractionalPart] = numberString.split(DECIMAL_POINT_CHAR)
 
   const fractionPartArray = fractionalPart.split('')
   const integerPartIsZero = integerPart === '0'
@@ -39,7 +45,7 @@ export const formatBtc = (
     .map((val, index) =>
       index === 2 || index === 5 ? (
         <>
-          {_thinSpace}
+          {spaceChar}
           {val}
         </>
       ) : (
@@ -54,7 +60,7 @@ export const formatBtc = (
     ))
 
   const formattedIntegerPart = integerPartIsZero ? deemphasize(integerPart) : integerPart
-  const formattedDecimalPoint = fractionalPartStartsWithZero ? deemphasize(_decimalPoint) : _decimalPoint
+  const formattedDecimalPoint = fractionalPartStartsWithZero ? deemphasize(DECIMAL_POINT_CHAR) : DECIMAL_POINT_CHAR
 
   return (
     <>
