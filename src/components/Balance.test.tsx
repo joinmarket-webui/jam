8c0e925adbd15846c50c81b1b3c3f0b9a9eb8461
theborakompanioni <theborakompanioni+github@gmail.com>
2023-11-10 10:10:39 +0100

    test(balance): use testid to get balance element

--- src/components/Balance.test.tsx
+++ src/components/Balance.test.tsx
@@ -25,7 +25,7 @@ describe('<Balance />', () => {
   it('should hide balance for SATS by default', () => {
     render(<Balance valueString={'123'} convertToUnit={SATS} />)
     expect(screen.getByText(`*****`)).toBeInTheDocument()
-    expect(screen.queryByText(`123`)).not.toBeInTheDocument()
+    expect(screen.queryByTestId(`sats-amount`)).not.toBeInTheDocument()
   })
 
   it('should render a string BTC value correctly as BTC', () => {
@@ -35,7 +35,7 @@ describe('<Balance />', () => {
 
   it('should render a string BTC value correctly as SATS', () => {
     render(<Balance valueString={'123.03224961'} convertToUnit={SATS} showBalance={true} />)
-    expect(screen.getByText(`12,303,224,961`)).toBeInTheDocument()
+    expect(screen.getByTestId(`sats-amount`)).toHaveTextContent(`12,303,224,961`)
   })
 
   it('should render a zero string BTC value correctly as BTC', () => {
@@ -45,7 +45,7 @@ describe('<Balance />', () => {
 
   it('should render a zero string BTC value correctly as SATS', () => {
     render(<Balance valueString={'0.00000000'} convertToUnit={SATS} showBalance={true} />)
-    expect(screen.getByText(`0`)).toBeInTheDocument()
+    expect(screen.getByTestId(`sats-amount`)).toHaveTextContent(`0`)
   })
 
   it('should render a large string BTC value correctly as BTC', () => {
@@ -55,7 +55,7 @@ describe('<Balance />', () => {
 
   it('should render a large string BTC value correctly as SATS', () => {
     render(<Balance valueString={'20999999.97690000'} convertToUnit={SATS} showBalance={true} />)
-    expect(screen.getByText(`2,099,999,997,690,000`)).toBeInTheDocument()
+    expect(screen.getByTestId(`sats-amount`)).toHaveTextContent(`2,099,999,997,690,000`)
   })
 
   it('should render a max string BTC value correctly as BTC', () => {
@@ -65,12 +65,12 @@ describe('<Balance />', () => {
 
   it('should render a max string BTC value correctly as SATS', () => {
     render(<Balance valueString={'21000000.00000000'} convertToUnit={SATS} showBalance={true} />)
-    expect(screen.getByText(`2,100,000,000,000,000`)).toBeInTheDocument()
+    expect(screen.getByTestId(`sats-amount`)).toHaveTextContent(`2,100,000,000,000,000`)
   })
 
   it('should render a string SATS value correctly as SATS', () => {
     render(<Balance valueString={'43000'} convertToUnit={SATS} showBalance={true} />)
-    expect(screen.getByText(`43,000`)).toBeInTheDocument()
+    expect(screen.getByTestId(`sats-amount`)).toHaveTextContent(`43,000`)
   })
 
   it('should render a string SATS value correctly as BTC', () => {
@@ -85,7 +85,7 @@ describe('<Balance />', () => {
 
   it('should render a zero string SATS value correctly as SATS', () => {
     render(<Balance valueString={'0'} convertToUnit={SATS} showBalance={true} />)
-    expect(screen.getByText(`0`)).toBeInTheDocument()
+    expect(screen.getByTestId(`sats-amount`)).toHaveTextContent(`0`)
   })
 
   it('should render a large string SATS value correctly as BTC', () => {
@@ -95,7 +95,7 @@ describe('<Balance />', () => {
 
   it('should render a large string SATS value correctly as SATS', () => {
     render(<Balance valueString={'2099999997690000'} convertToUnit={SATS} showBalance={true} />)
-    expect(screen.getByText(`2,099,999,997,690,000`)).toBeInTheDocument()
+    expect(screen.getByTestId(`sats-amount`)).toHaveTextContent(`2,099,999,997,690,000`)
   })
 
   it('should render a max string SATS value correctly as BTC', () => {
@@ -105,72 +105,72 @@ describe('<Balance />', () => {
 
   it('should render a max string SATS value correctly as SATS', () => {
     render(<Balance valueString={'2100000000000000'} convertToUnit={SATS} showBalance={true} />)
-    expect(screen.getByText(`2,100,000,000,000,000`)).toBeInTheDocument()
+    expect(screen.getByTestId(`sats-amount`)).toHaveTextContent(`2,100,000,000,000,000`)
   })
 
   it('should toggle visibility of initially hidden balance on click by default', () => {
     render(<Balance valueString={`21`} convertToUnit={SATS} showBalance={false} />)
-    expect(screen.queryByText(`21`)).not.toBeInTheDocument()
+    expect(screen.queryByTestId(`sats-amount`)).not.toBeInTheDocument()
     expect(screen.getByText(`*****`)).toBeInTheDocument()
 
     act(() => {
       user.click(screen.getByText(`*****`))
     })
 
-    expect(screen.getByText(`21`)).toBeInTheDocument()
+    expect(screen.getByTestId(`sats-amount`)).toBeInTheDocument()
     expect(screen.queryByText(`*****`)).not.toBeInTheDocument()
 
     act(() => {
-      user.click(screen.getByText(`21`))
+      user.click(screen.getByTestId(`sats-amount`))
     })
 
-    expect(screen.queryByText(`21`)).not.toBeInTheDocument()
+    expect(screen.queryByTestId(`sats-amount`)).not.toBeInTheDocument()
     expect(screen.getByText(`*****`)).toBeInTheDocument()
   })
 
   it('should NOT toggle visibility of initially hidden balance on click when disabled via flag', () => {
     render(<Balance valueString={`21`} convertToUnit={SATS} showBalance={false} enableVisibilityToggle={false} />)
-    expect(screen.queryByText(`21`)).not.toBeInTheDocument()
+    expect(screen.queryByTestId(`sats-amount`)).not.toBeInTheDocument()
     expect(screen.getByText(`*****`)).toBeInTheDocument()
 
     act(() => {
       user.click(screen.getByText(`*****`))
     })
 
-    expect(screen.queryByText(`21`)).not.toBeInTheDocument()
+    expect(screen.queryByTestId(`sats-amount`)).not.toBeInTheDocument()
     expect(screen.getByText(`*****`)).toBeInTheDocument()
   })
 
   it('should NOT toggle visibility of initially visible balance on click by default', () => {
     render(<Balance valueString={`21`} convertToUnit={SATS} showBalance={true} />)
-    expect(screen.getByText(`21`)).toBeInTheDocument()
+    expect(screen.getByTestId(`sats-amount`)).toBeInTheDocument()
     expect(screen.queryByText(`*****`)).not.toBeInTheDocument()
 
     act(() => {
-      user.click(screen.getByText(`21`))
+      user.click(screen.getByTestId(`sats-amount`))
     })
 
-    expect(screen.getByText(`21`)).toBeInTheDocument()
+    expect(screen.getByTestId(`sats-amount`)).toBeInTheDocument()
     expect(screen.queryByText(`*****`)).not.toBeInTheDocument()
   })
 
   it('should toggle visibility of initially visible balance on click when enabled via flag', () => {
     render(<Balance valueString={`21`} convertToUnit={SATS} showBalance={true} enableVisibilityToggle={true} />)
-    expect(screen.getByText(`21`)).toBeInTheDocument()
+    expect(screen.getByTestId(`sats-amount`)).toBeInTheDocument()
     expect(screen.queryByText(`*****`)).not.toBeInTheDocument()
 
     act(() => {
-      user.click(screen.getByText(`21`))
+      user.click(screen.getByTestId(`sats-amount`))
     })
 
-    expect(screen.queryByText(`21`)).not.toBeInTheDocument()
+    expect(screen.queryByTestId(`sats-amount`)).not.toBeInTheDocument()
     expect(screen.getByText(`*****`)).toBeInTheDocument()
 
     act(() => {
       user.click(screen.getByText(`*****`))
     })
 
-    expect(screen.getByText(`21`)).toBeInTheDocument()
+    expect(screen.getByTestId(`sats-amount`)).toBeInTheDocument()
     expect(screen.queryByText(`*****`)).not.toBeInTheDocument()
   })
 })
