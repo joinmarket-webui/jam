27e687c9cf34fdd1f92b8c675c6579b4759f9389
Zachary Johnson <zachary.business@tutanota.com>
2024-01-25 12:10:07 -0500

    feat(ui): autofocus next input when confirming seed phrase backup (#718)

--- src/components/MnemonicWordInput.tsx
+++ src/components/MnemonicWordInput.tsx
@@ -3,19 +3,32 @@ import { useTranslation } from 'react-i18next'
 import styles from './MnemonicWordInput.module.css'
 
 interface MnemonicWordInputProps {
+  forwardRef: (el: HTMLInputElement) => void
   index: number
   value: string
   setValue: (value: string, index: number) => void
   isValid?: boolean
   disabled?: boolean
+  onFocus?: () => void
+  autoFocus?: boolean
 }
 
-const MnemonicWordInput = ({ index, value, setValue, isValid, disabled }: MnemonicWordInputProps) => {
+const MnemonicWordInput = ({
+  forwardRef,
+  index,
+  value,
+  setValue,
+  isValid,
+  disabled,
+  onFocus,
+  autoFocus,
+}: MnemonicWordInputProps) => {
   const { t } = useTranslation()
   return (
     <rb.InputGroup>
       <rb.InputGroup.Text className={styles.seedwordIndexBackup}>{index + 1}.</rb.InputGroup.Text>
       <rb.Form.Control
+        ref={forwardRef}
         type="text"
         placeholder={`${t('create_wallet.placeholder_seed_word_input')} ${index + 1}`}
         value={value}
@@ -24,6 +37,8 @@ const MnemonicWordInput = ({ index, value, setValue, isValid, disabled }: Mnemon
         disabled={disabled}
         isInvalid={isValid === false && value.length > 0}
         isValid={isValid === true}
+        onFocus={onFocus}
+        autoFocus={autoFocus}
         required
       />
     </rb.InputGroup>
