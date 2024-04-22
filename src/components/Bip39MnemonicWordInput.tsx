f44edf230d327420ddb76ba5a17fb84d48274967
theborakompanioni <theborakompanioni+github@gmail.com>
2024-04-22 10:23:02 +0200

    chore(mnemonic): pass given isValid to input

--- src/components/Bip39MnemonicWordInput.tsx
+++ src/components/Bip39MnemonicWordInput.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import { useMemo } from 'react'
 import MnemonicWordInput, { MnemonicWordInputProps } from './MnemonicWordInput'
 import { MNEMONIC_WORDS } from '../constants/bip39words'
 
-const Bip39MnemonicWordInput: React.FC<MnemonicWordInputProps> = (props) => {
-  const { value } = props
-  const isBip39Value = MNEMONIC_WORDS.includes(value)
+const Bip39MnemonicWordInput = ({ value, ...props }: MnemonicWordInputProps) => {
+  const isBip39Value = useMemo(() => MNEMONIC_WORDS.includes(value), [value])
 
-  return <MnemonicWordInput {...props} isValid={isBip39Value} />
+  return <MnemonicWordInput {...props} value={value} isValid={isBip39Value && (props.isValid ?? true)} />
 }
 
 export default Bip39MnemonicWordInput
