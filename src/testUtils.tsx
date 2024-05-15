e02bb2a5d2de65d128700a11fc413bab90c58ef2
theborakompanioni <theborakompanioni+github@gmail.com>
2024-05-15 15:44:39 +0200

    build(deps): update testing libs dependencies
     @testing-library/jest-dom    ^6.2.0  →    ^6.4.5
     @testing-library/react      ^14.1.2  →   ^15.0.7
     @types/jest                ^29.5.11  →  ^29.5.12

--- src/testUtils.tsx
+++ src/testUtils.tsx
@@ -1,4 +1,4 @@
-import { StrictMode, ReactElement } from 'react'
+import { StrictMode, ReactNode } from 'react'
 import { render, RenderOptions } from '@testing-library/react'
 import { I18nextProvider } from 'react-i18next'
 import { WalletProvider } from './context/WalletContext'
@@ -8,7 +8,7 @@ import { SettingsProvider } from './context/SettingsContext'
 import { WebsocketProvider } from './context/WebsocketContext'
 import i18n from './i18n/testConfig'
 
-const AllTheProviders = ({ children }: { children: ReactElement }) => {
+const AllTheProviders = ({ children }: { children: ReactNode }) => {
   return (
     <StrictMode>
       <I18nextProvider i18n={i18n}>
@@ -26,7 +26,7 @@ const AllTheProviders = ({ children }: { children: ReactElement }) => {
   )
 }
 
-const customRender = (ui: ReactElement, options?: RenderOptions) => render(ui, { wrapper: AllTheProviders, ...options })
+const customRender = (ui: ReactNode, options?: RenderOptions) => render(ui, { wrapper: AllTheProviders, ...options })
 
 // re-export everything
 export * from '@testing-library/react'
