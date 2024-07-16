b4f8a56baa5e1f4bbad1cd437a3f08172d9db026
Thebora Kompanioni <theborakompanioni@users.noreply.github.com>
2024-07-16 15:26:17 +0200

    feat(config): ability to customize "max sweep fee change" setting (#793)

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -23,3 +23,6 @@ export const JM_API_AUTH_TOKEN_EXPIRY: Milliseconds = Math.round(0.5 * 60 * 60 *
 // cap of dusty offer minsizes ("has dusty minsize, capping at 27300")
 // See: https://github.com/JoinMarket-Org/joinmarket-clientserver/blob/v0.9.11/src/jmclient/configure.py#L70 (last check on 2024-04-22 of v0.9.11)
 export const JM_DUST_THRESHOLD = 27_300
+
+// See: https://github.com/JoinMarket-Org/joinmarket-clientserver/blob/v0.9.11/src/jmclient/configure.py#L321 (last check on 2024-07-09 of v0.9.11)
+export const JM_MAX_SWEEP_FEE_CHANGE_DEFAULT = 0.8
