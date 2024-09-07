2fb840902d5b019604acef0ecd833ae7cc4dbdf3
Nischal Shetty <nischal2004coc@gmail.com>
2024-09-07 14:16:44 +0530

    build fix

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -16,7 +16,7 @@ import languages from '../i18n/languages'
 import styles from './Settings.module.css'
 import SeedModal from './settings/SeedModal'
 import FeeConfigModal from './settings/FeeConfigModal'
-import { isDebugFeatureEnabled, isDevMode } from '../constants/debugFeatures'
+import { isDevMode } from '../constants/debugFeatures'
 import { isFeatureEnabled } from '../constants/features'
 import { CurrentWallet } from '../context/WalletContext'
 
