3ec72b187d78def597399ccb49573bfeae855a9a
Nischal Shetty <nischal2004coc@gmail.com>
2024-09-07 18:48:25 +0530

    feat(rescan): ability to rescan timechain (#838)

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -16,7 +16,7 @@ import languages from '../i18n/languages'
 import styles from './Settings.module.css'
 import SeedModal from './settings/SeedModal'
 import FeeConfigModal from './settings/FeeConfigModal'
-import { isDebugFeatureEnabled, isDevMode } from '../constants/debugFeatures'
+import { isDevMode } from '../constants/debugFeatures'
 import { isFeatureEnabled } from '../constants/features'
 import { CurrentWallet } from '../context/WalletContext'
 
@@ -213,16 +213,10 @@ export default function Settings({ wallet, stopWallet }: SettingsProps) {
             {t('settings.button_switch_wallet')}
           </Link>
 
-          {serviceInfo && isFeatureEnabled('rescanChain', serviceInfo) && isDebugFeatureEnabled('rescanChainPage') && (
-            <Link
-              to={routes.rescanChain}
-              className={`btn btn-outline-dark ${styles['settings-btn']} position-relative`}
-            >
+          {serviceInfo && isFeatureEnabled('rescanChain', serviceInfo) && (
+            <Link to={routes.rescanChain} className={`btn btn-outline-dark ${styles['settings-btn']}`}>
               <Sprite symbol="block" width="24" height="24" />
-              Rescan chain
-              <span className="position-absolute top-50 start-0 translate-middle badge rounded-pill bg-warning">
-                dev
-              </span>
+              {t('settings.rescan_chain')}
             </Link>
           )}
 
