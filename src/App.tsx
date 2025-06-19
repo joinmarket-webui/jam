4924d7abb4eb30ebb655d31ab2d181a58e2fd132
theborakompanioni <theborakompanioni+github@gmail.com>
2025-06-19 14:55:38 +0200

    chore: enable close button on toasts

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ function App() {
             />
             <Route path="*" element={<Navigate to="/login" replace />} />
           </Routes>
-          <Toaster />
+          <Toaster closeButton />
         </Router>
       </QueryClientProvider>
     </>
