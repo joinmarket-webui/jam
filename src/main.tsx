f77f5bbfb2a1b52fb08cbc825ea9fe39a9866034
Nischal Shetty <nischal2004coc@gmail.com>
2025-06-27 21:44:11 +0530

    feat: initialize i18n with translations

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from '@/App.tsx'
+import '@/i18n/config'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
