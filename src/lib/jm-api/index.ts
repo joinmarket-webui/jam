5434c48e958c7ceac66b2afbc5f032dd0c94a8f7
theborakompanioni <theborakompanioni+github@gmail.com>
2025-09-10 00:58:31 +0200

    chore(v2): use updated joinmarket-api-ts client

--- src/lib/jm-api/index.ts
+++ src/lib/jm-api/index.ts
@@ -1,4 +1,5 @@
-import { createClient as __createClient, createConfig } from '@hey-api/client-fetch'
+import { createClient as __createClient, createConfig } from './generated/client/client'
+
 import type { ClientOptions } from '@/lib/jm-api/generated/client'
 
 export const createClient = (options: ClientOptions) => __createClient(createConfig<ClientOptions>(options))
