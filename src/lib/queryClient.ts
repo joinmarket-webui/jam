a9fc74717979fe7439a7ff424d48ae2efefb8f81
theborakompanioni <theborakompanioni+github@gmail.com>
2025-09-18 15:05:13 +0200

    chore(v2): remove client default stale time

--- src/lib/queryClient.ts
+++ src/lib/queryClient.ts
@@ -3,7 +3,6 @@ import { QueryClient } from '@tanstack/react-query'
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 1_000 * 60 * 5,
       refetchOnWindowFocus: false,
       retryDelay: (attemptIndex) => Math.min(1_000 * 2 ** attemptIndex, 30_000),
       retry: false,
