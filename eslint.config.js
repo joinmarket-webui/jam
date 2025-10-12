32d96298e770131e25659628994efa9d1bf6e49b
theborakompanioni <theborakompanioni+github@gmail.com>
2025-10-12 15:51:04 +0200

    build(v2): use eslint defineConfig
    see: https://typescript-eslint.io/packages/typescript-eslint/#migrating-to-defineconfig

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,11 @@ import js from '@eslint/js'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import storybook from 'eslint-plugin-storybook'
+import { defineConfig } from 'eslint/config'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
+export default defineConfig(
   { ignores: ['dist', './.storybook/**', 'src/lib/jm-api/**'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
