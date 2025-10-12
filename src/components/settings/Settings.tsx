8b380a72dc4460f6428aeab0a3b8120a482270ff
theborakompanioni <theborakompanioni+github@gmail.com>
2025-10-12 21:14:45 +0200

    chore: SatSymbol and BtcSymbol

--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -1,21 +1,18 @@
 import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import {
-  Eye,
-  EyeOff,
-  Bitcoin,
-  Sun,
-  Moon,
-  DollarSign,
-  Key,
-  Unlock,
-  RotateCcw,
-  RefreshCw,
-  FileText,
-  Book,
-  // TODO: Remove depricated imports(company logos) from lucide react and use https://simpleicons.org directly
-  Github,
-  ExternalLink,
+  EyeIcon,
+  EyeOffIcon,
+  SunIcon,
+  MoonIcon,
+  DollarSignIcon,
+  KeyIcon,
+  UnlockIcon,
+  RotateCcwIcon,
+  RefreshCwIcon,
+  FileTextIcon,
+  BookIcon,
+  ExternalLinkIcon,
   TerminalIcon,
 } from 'lucide-react'
 import { useTheme } from 'next-themes'
@@ -23,6 +20,7 @@ import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
 import { useStore } from 'zustand'
+import { BtcSymbol, SatSymbol } from '@/components/CurrencySymbol'
 import { useJamDisplayContext } from '@/components/layout/display-mode-context'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Separator } from '@/components/ui/separator'
@@ -88,7 +86,7 @@ export const SettingsPage = ({ walletFileName }: SettingPageProps) => {
         </CardHeader>
         <CardContent className="space-y-0">
           <SettingSwitch
-            icon={isPrivate ? Eye : EyeOff}
+            icon={isPrivate ? EyeIcon : EyeOffIcon}
             title={t(isPrivate ? 'settings.show_balance' : 'settings.hide_balance')}
             checked={isPrivate}
             onCheckedChange={togglePrivacyMode}
@@ -96,7 +94,9 @@ export const SettingsPage = ({ walletFileName }: SettingPageProps) => {
           />
           <Separator className="opacity-50" />
           <SettingSwitch
-            icon={Bitcoin}
+            renderIcon={({ className }) =>
+              currency === 'btc' ? <BtcSymbol className={className} /> : <SatSymbol className={className} />
+            }
             title={t(currency === 'btc' ? 'settings.use_btc' : 'settings.use_sats')}
             checked={currency === 'btc'}
             onCheckedChange={toggleCurrencyUnit}
@@ -104,7 +104,7 @@ export const SettingsPage = ({ walletFileName }: SettingPageProps) => {
           />
           <Separator className="opacity-50" />
           <SettingSwitch
-            icon={resolvedTheme === 'dark' ? Sun : Moon}
+            icon={resolvedTheme === 'dark' ? SunIcon : MoonIcon}
             title={resolvedTheme === 'dark' ? t('settings.use_light_theme') : t('settings.use_dark_theme')}
             checked={resolvedTheme === 'dark'}
             onCheckedChange={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
@@ -122,7 +122,7 @@ export const SettingsPage = ({ walletFileName }: SettingPageProps) => {
         </CardHeader>
         <CardContent>
           <SettingItem
-            icon={DollarSign}
+            icon={DollarSignIcon}
             title={t('settings.show_fee_config')}
             action={async () => {
               setShowFeeLimitDialog(true)
@@ -138,24 +138,24 @@ export const SettingsPage = ({ walletFileName }: SettingPageProps) => {
         </CardHeader>
         <CardContent className="space-y-0">
           <SettingItem
-            icon={Key}
+            icon={KeyIcon}
             title={t('settings.show_seed')}
             action={async () => setShowSeedDialog(true)}
             disabled={hashedPassword === undefined}
           />
           <Separator className="opacity-50" />
           <SettingItem
-            icon={Unlock}
+            icon={UnlockIcon}
             title={t('settings.button_lock_wallet')}
             action={handleLockWallet}
             disabled={lockWalletQuery.isFetching}
           />
           <Separator className="opacity-50" />
-          <SettingsLink icon={RotateCcw} title={t('settings.button_switch_wallet')} to={routes.switchWallet} />
+          <SettingsLink icon={RotateCcwIcon} title={t('settings.button_switch_wallet')} to={routes.switchWallet} />
           <Separator className="opacity-50" />
-          <SettingsLink icon={RefreshCw} title={t('settings.rescan_chain')} to={routes.rescan} />
+          <SettingsLink icon={RefreshCwIcon} title={t('settings.rescan_chain')} to={routes.rescan} />
           <Separator className="opacity-50" />
-          <SettingsLink icon={FileText} title={t('settings.show_logs')} to={routes.logs} disabled={isLogsEnabled} />
+          <SettingsLink icon={FileTextIcon} title={t('settings.show_logs')} to={routes.logs} disabled={isLogsEnabled} />
         </CardContent>
       </Card>
 
@@ -179,7 +179,7 @@ export const SettingsPage = ({ walletFileName }: SettingPageProps) => {
                 <p className="text-sm font-medium">{t('settings.matrix')}</p>
               </div>
             </div>
-            <ExternalLink className="text-muted-foreground h-3 w-3" />
+            <ExternalLinkIcon className="text-muted-foreground h-3 w-3" />
           </a>
           <Separator className="opacity-50" />
           <a
@@ -196,7 +196,7 @@ export const SettingsPage = ({ walletFileName }: SettingPageProps) => {
                 <p className="text-sm font-medium">{t('settings.telegram')}</p>
               </div>
             </div>
-            <ExternalLink className="text-muted-foreground h-3 w-3" />
+            <ExternalLinkIcon className="text-muted-foreground h-3 w-3" />
           </a>
         </CardContent>
       </Card>
@@ -207,10 +207,10 @@ export const SettingsPage = ({ walletFileName }: SettingPageProps) => {
           <CardTitle className="text-base font-medium">{t('settings.section_title_dev')}</CardTitle>
         </CardHeader>
         <CardContent className="space-y-0">
-          <SettingsLink icon={Book} title={t('settings.documentation')} to="https://jamdocs.org" external={true} />
+          <SettingsLink icon={BookIcon} title={t('settings.documentation')} to="https://jamdocs.org" external={true} />
           <Separator className="opacity-50" />
           <SettingsLink
-            icon={Github}
+            renderIcon={({ className }) => <GitHubIcon className={className} />}
             title={t('settings.github')}
             to="https://github.com/joinmarket-webui/jam"
             external={true}
@@ -257,3 +257,10 @@ export const SettingsPage = ({ walletFileName }: SettingPageProps) => {
     </div>
   )
 }
+
+const GitHubIcon = ({ className }: { className: string }) => (
+  <svg className={className} fill="currentColor" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <title>GitHub</title>
+    <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+  </svg>
+)
