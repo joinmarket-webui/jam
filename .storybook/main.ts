c4fd619af33d230dacfdb825dd7738d1f4cc1032
theborakompanioni <theborakompanioni+github@gmail.com>
2025-11-16 13:24:04 +0100

    chore: disable storybook telemetry

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,5 +7,9 @@ const config: StorybookConfig = {
     name: '@storybook/react-vite',
     options: {},
   },
+  core: {
+    disableTelemetry: true,
+    enableCrashReports: false,
+  },
 }
 export default config
