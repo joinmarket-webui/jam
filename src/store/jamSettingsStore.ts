ddafccca172951ca266e702e885cddc30307ead0
theborakompanioni <theborakompanioni+github@gmail.com>
2026-01-11 05:22:54 +0100

    chore: initially display cheatsheet

--- src/store/jamSettingsStore.ts
+++ src/store/jamSettingsStore.ts
@@ -1,12 +1,13 @@
 import { createStore } from 'zustand'
 import { persist, createJSONStorage } from 'zustand/middleware'
 import { isDevMode } from '@/constants/debugFeatures'
-import type { Currency } from '@/types/global'
+import type { Currency, Milliseconds } from '@/types/global'
 
 export type JamSettings = {
   developerMode: boolean
   privateMode: boolean
   currencyUnit: Currency
+  cheatsheetLastDisplayTime: Milliseconds | undefined
 }
 
 interface JamSettingsStoreState {
@@ -19,6 +20,7 @@ const initial: JamSettings = {
   developerMode: isDevMode(),
   privateMode: false,
   currencyUnit: 'sats',
+  cheatsheetLastDisplayTime: undefined,
 }
 
 export const jamSettingsStore = createStore<JamSettingsStoreState>()(
