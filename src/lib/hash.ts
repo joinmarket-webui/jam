1b974ff2d3e1de00b36fab0872027700c994b265
theborakompanioni <theborakompanioni+github@gmail.com>
2026-01-18 16:20:26 +0100

    chore: change password hash to pbkdf2-sha512

--- src/lib/hash.ts
+++ src/lib/hash.ts
@@ -1,10 +1,11 @@
 import { pbkdf2Async } from '@noble/hashes/pbkdf2.js'
-import { sha256 } from '@noble/hashes/sha2.js'
+import { sha512 } from '@noble/hashes/sha2.js'
 
+// see https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html#pbkdf2 (last check: 2026-01)
 export const DEFAULT_PBKDF_ITERATIONS = 210_000
 
 /**
- * Securely hashes a password using PBKDF2 with SHA-256.
+ * Securely hashes a password using PBKDF2 with SHA-512.
  * @param password The password to hash.
  * @param salt A salt value (should be unique per user).
  * @param iterations Number of PBKDF2 iterations (default: 210_000).
@@ -20,7 +21,7 @@ export async function hashPassword(
     const passwordBuffer = new TextEncoder().encode(password)
     const saltBuffer = new TextEncoder().encode(salt)
 
-    const derivedKey = await pbkdf2Async(sha256, passwordBuffer, saltBuffer, { c: iterations, dkLen: 32 })
+    const derivedKey = await pbkdf2Async(sha512, passwordBuffer, saltBuffer, { c: iterations, dkLen: 32 })
 
     // Convert Uint8Array to hex string
     return Array.from(derivedKey)
