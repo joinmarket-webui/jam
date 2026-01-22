22966982eedd2bc884653e68c85da6b1cdb4e7b9
theborakompanioni <theborakompanioni+github@gmail.com>
2026-01-22 12:38:54 +0100

    chore: active wallet indicator on login page

--- src/components/login/LoginPage.tsx
+++ src/components/login/LoginPage.tsx
@@ -157,7 +157,7 @@ const LoginPage = () => {
                   ) : (
                     <LoginForm
                       wallets={wallets}
-                      preselectedWallet={activeWalletOrNull ?? undefined}
+                      activeWallet={activeWalletOrNull ?? undefined}
                       disabled={login.isPending || listWalletsFetching}
                       isSubmitting={login.isPending}
                       onSubmit={handleSubmit}
--- src/stories/jam/pages/login/LoginForm.stories.tsx
+++ src/stories/jam/pages/login/LoginForm.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react-vite'
 import { LoginForm } from '@/components/login/LoginForm'
+import type { WalletFileName } from '@/lib/utils'
 
 const meta: Meta<typeof LoginForm> = {
   title: 'Page/Login/LoginForm',
@@ -10,18 +11,54 @@ export default meta
 
 type Story = StoryObj<typeof LoginForm>
 
+const WALLETS = [
+  'Satoshi.jmdat',
+  'Wallet With Whitespaces.jmdat',
+  'a.jmdat',
+  'A very long wallet name that should be shortened but show start and end of name.jmdat',
+] as WalletFileName[]
+
 export const Default: Story = {
   args: {
     loading: false,
-    wallets: ['Satoshi.jmdat'],
+    wallets: WALLETS,
+    activeWallet: undefined,
     isSubmitting: false,
     disabled: false,
     onSubmit: async () => alert('Submit clicked!'),
+  },
+  argTypes: {
+    activeWallet: {
+      control: 'select',
+      options: [...WALLETS, 'None'],
+    },
   },
 }
 
 export const Loading: Story = {
   args: {
     loading: true,
+  },
+}
+
+export const OneWallet: Story = {
+  args: {
+    loading: false,
+    wallets: ['Satoshi.jmdat'],
+    activeWallet: undefined,
+    isSubmitting: false,
+    disabled: false,
+    onSubmit: async () => alert('Submit clicked!'),
+  },
+}
+
+export const ActiveWallet: Story = {
+  args: {
+    loading: false,
+    wallets: WALLETS,
+    activeWallet: WALLETS[0],
+    isSubmitting: false,
+    disabled: false,
+    onSubmit: async () => alert('Submit clicked!'),
   },
 }
