5dbb5dbdf8b4b5c6a77d986d6da097bb94c91a07
theborakompanioni <theborakompanioni+github@gmail.com>
2026-04-29 10:10:53 +0200

    cleanup: remove legacy from v2 merge

--- eslint.config.js
+++ eslint.config.js
@@ -11,8 +11,7 @@ import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default defineConfig(
-  // TODO(v2-cleanup): remove `build` ignore after v2 branch is merged; legacy `devel` (CRA) outputs `build/`
-  { ignores: ['dist', 'build', './.storybook/**', './storybook-static/**'] },
+  { ignores: ['dist', './.storybook/**', './storybook-static/**'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
