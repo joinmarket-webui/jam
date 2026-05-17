165d0c4b9cff016357a7816d7c21d9bec256d5c8
Thebora Kompanioni <theborakompanioni@users.noreply.github.com>
2026-05-17 11:02:49 +0200

    chore(ui): improve activity indicators (#1249)
    Co-authored-by: Kishore B <85435355+kishore08-07@users.noreply.github.com>

---
+++ src/components/ui/jam/ActivityIndicator.tsx
@@ -0,0 +1,31 @@
+import type { PropsWithChildren } from 'react'
+import { cn } from '@/lib/utils'
+
+type ActivityIndicatorProps = {
+  active?: boolean
+  animateEnter?: boolean
+  className?: string
+}
+
+export const ActivityIndicator = ({ active = true, animateEnter = false, className }: ActivityIndicatorProps) => {
+  return (
+    <span
+      className={cn('relative flex size-2', className, {
+        hidden: !active,
+        'animate-in fade-in-0 zoom-in-0 duration-2000': animateEnter,
+      })}
+    >
+      <span className="light:bg-green-500 absolute inline-flex h-full w-full animate-ping rounded-full bg-green-200 opacity-75 [animation-duration:_2.1s]" />
+      <span className="light:bg-green-600 relative inline-flex size-2 rounded-full bg-green-300" />
+    </span>
+  )
+}
+
+export const WithActivityIndicator = ({ children, ...props }: PropsWithChildren<ActivityIndicatorProps>) => {
+  return (
+    <span className="relative">
+      {children}
+      <ActivityIndicator animateEnter {...props} className={cn('absolute -top-0.5 -right-3', props.className)} />
+    </span>
+  )
+}
---
+++ src/stories/jam/ActivityIndicator.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+import { ActivityIndicator, WithActivityIndicator } from '@/components/ui/jam/ActivityIndicator'
+
+const meta: Meta<typeof ActivityIndicator> = {
+  title: 'Jam/ActivityIndicator',
+  component: ActivityIndicator,
+  tags: ['autodocs'],
+  args: {
+    animateEnter: true,
+  },
+  argTypes: {
+    animateEnter: {
+      description: 'Enable enter animation',
+      control: 'boolean',
+    },
+  },
+}
+export default meta
+
+type Story = StoryObj<typeof ActivityIndicator>
+
+export const Default: Story = {
+  args: {},
+}
+
+export const ElementWithActivityIndicator: Story = {
+  render: () => <WithActivityIndicator>Activity Text</WithActivityIndicator>,
+}
