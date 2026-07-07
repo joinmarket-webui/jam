1b5d1cf6e687665164eeed770cc07125f2c48c08
theborakompanioni <theborakompanioni+github@gmail.com>
2026-07-07 16:49:03 +0200

    Revert "dev(ui): add ugly beta warning header (#1317)"
    This reverts commit e7f317aa49e99cc13b5722fabd8b79eb9f64d76d.

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -104,7 +104,7 @@ function SidebarProvider({
               ...style,
             } as React.CSSProperties
           }
-          className={cn('group/sidebar-wrapper has-data-[variant=inset]:bg-sidebar flex w-full flex-1', className)}
+          className={cn('group/sidebar-wrapper has-data-[variant=inset]:bg-sidebar flex min-h-svh w-full', className)}
           {...props}
         >
           {children}
