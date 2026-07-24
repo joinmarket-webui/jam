dfbfee7efb917c91b86c8865f64647003fa1714e
theborakompanioni <theborakompanioni+github@gmail.com>
2026-07-24 23:13:36 +0200

    fix(ui): scrollable area in WalletJarDetails

--- src/components/wallet/WalletJarsDetailsContent.tsx
+++ src/components/wallet/WalletJarsDetailsContent.tsx
@@ -358,7 +358,7 @@ export const WalletJarsDetailsContent = ({
         </TabsList>
       </Tabs>
 
-      <Tabs defaultValue="utxos" className="flex min-h-0 flex-1 flex-col gap-4">
+      <Tabs defaultValue="utxos" className="flex flex-1 flex-col gap-4">
         <TabsList className="mx-auto flex items-center gap-2">
           <TabsTrigger value="utxos" className="cursor-pointer" disabled={!enabled}>
             {t('jar_details.title_tab_utxos')}
