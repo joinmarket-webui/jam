1869d2d16644738ed2c1d4758ff1e5eb810aadeb
theborakompanioni <theborakompanioni+github@gmail.com>
2026-09-23 22:16:27 +0200

    chore(cleanup): remove unused function scrollToTop

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -300,17 +300,6 @@ export const median = (values: number[]): number | null => {
   return Number.isFinite(median) ? median : null
 }
 
-/**
- * Scrolls to the top of the page.
- *
- * Hint: There is a small delay before the scrolling is initiated,
- * in order to mitigate some weird browser behaviour, where it
- * did not properly work without a timeout.
- */
-export const scrollToTop = (options?: ScrollOptions) => {
-  return setTimeout(() => window.scrollTo({ behavior: 'smooth', ...options, top: 0, left: 0 }), 21)
-}
-
 export const scrollIntoView = <T extends HTMLElement>(
   targetElement: RefObject<T | null> | null,
   options?: ScrollIntoViewOptions,
